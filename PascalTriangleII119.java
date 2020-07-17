082e57dea0f2afd6c04ade1e5f882834d42fd6ea
kqiu10 <qiukan97@gmail.com>
2020-07-17 19:01:18 -0400

    finish119

--- PascalTriangleII119.java
+++ PascalTriangleII119.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangleII119 {
-    public List<Integer> getRow(int rowIndex) {
-
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < rowIndex+1; i++) {
+            list.add(0,1);
+            for(int j = 1; j < list.size()-1; j++ ) {
+                list.set(j, list.get(j)+list.get(j+1));
+            }
+        }
+        return list;
     }
+    public static void main(String[] args){
+        System.out.println(getRow(3));
+    }
+
+
     
 }
