e777a6a3586ba8a444168ed39deb9a272e108c36
kqiu10 <qiukan97@gmail.com>
2020-07-18 20:49:20 -0400

    finish 27

---
+++ RemoveElement27.java
@@ -0,0 +1,32 @@
+/**
+ * Description:
+ * Package Name : PACKAGE_NAME;
+ * File name : RemoveElement27;
+ * Creator: Kane;
+ * Date: 7/18/20
+ */
+public class RemoveElement27 {
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (val != nums[i]) {
+                nums[res++] = nums[i];
+            }
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,2};
+        removeElement(nums,2);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+
+        }
+
+    }
+}
