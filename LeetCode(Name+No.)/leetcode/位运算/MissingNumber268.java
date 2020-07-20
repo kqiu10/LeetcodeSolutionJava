c66a4db65d0613ab83915a155d7f56b5afdb1c37
kqiu10 <qiukan97@gmail.com>
2020-07-20 15:16:12 -0400

    finish 268

---
+++ LeetCode(Name+No.)/leetcode/位运算/MissingNumber268.java
@@ -0,0 +1,24 @@
+package leetcode.位运算;
+/**
+ * Description: TODO
+ * Package Name : leetcode.位运算;
+ * File name : MissingNumber268;
+ * Creator: Kane;
+ * Date: 7/20/20
+ */
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ */
+public class MissingNumber268 {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i ^ nums[i];
+
+        }
+        return res;
+
+    }
+}
