7abaf1d720b4c44bb52688cad3a80dbd26e6b181
kqiu10 <qiukan97@gmail.com>
2020-07-22 18:23:26 -0400

    Update AddBinary67.java

--- LeetCode(Name+No.)/leetcode/Mathmatics数学题/AddBinary67.java
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/AddBinary67.java
@@ -7,13 +7,42 @@ package leetcode.Mathmatics数学题;
  * Date: 7/21/20
  */
 
+import jdk.dynalink.beans.StaticClass;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 /**
- * Time complexity:O();
- * Space complexity: O();
- */
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Ex
+ * a = "11"
+ * b = "1"
+ * return "100"
+ * */
 public class AddBinary67 {
-    public String addBinary(String a, String b) {
+    public static String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        return "1";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int reminder = 0;
+        while (i >=0 || j >= 0) {
+            int sum = reminder;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            sb.append(sum % 2);
+            reminder = sum / 2;
+        }
+        if (reminder != 0) {
+            sb.append(reminder);
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        addBinary("101","1");
     }
 }
