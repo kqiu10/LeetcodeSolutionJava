200e37d8b85426a4241d950f01f95519696ea93d
kqiu10 <qiukan97@gmail.com>
2020-07-25 01:41:23 -0400

    finish 415

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/MultiplyStrings43.java
@@ -0,0 +1,18 @@
+package leetcode.Mathmatics数学题;
+/**
+ * Description: TODO
+ * Package Name : leetcode.Mathmatics数学题;
+ * File name : MultiplyStrings;
+ * Creator: Kane;
+ * Date: 7/25/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ */
+public class MultiplyStrings43 {
+    public String multiply(String num1, String num2) {
+
+    }
+}
