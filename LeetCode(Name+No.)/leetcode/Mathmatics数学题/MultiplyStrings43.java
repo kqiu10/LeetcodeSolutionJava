fddeb57af05d71f9aaf43d94095b9c65d238586c
kqiu10 <qiukan97@gmail.com>
2020-07-27 01:45:07 -0400

    Update MultiplyStrings43.java

--- LeetCode(Name+No.)/leetcode/Mathmatics数学题/MultiplyStrings43.java
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/MultiplyStrings43.java
@@ -8,11 +8,38 @@ package leetcode.Mathmatics数学题;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
+ * num1.length() == n;
+ * num2.length() == m;
+ * Time complexity:O(n * m);
+ * Space complexity: O(n + m);
  */
 public class MultiplyStrings43 {
-    public String multiply(String num1, String num2) {
+    public static String multiply(String num1, String num2) {
+        if (num1.length() == 0 || num2.length() == 0) {
+            return null;
+        }
+        int[] digits = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = product + digits[p2];
+                digits[p1] += sum / 10;
+                digits[p2] = sum % 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int digit : digits) {
+            if (!(digit == 0 && sb.length() == 0)) {
+                sb.append(digit);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply("123","456"));
     }
 }
