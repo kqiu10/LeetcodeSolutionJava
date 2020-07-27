ffb3da5f20a761e23ff8f2b605ec5db0f3bfee79
kqiu10 <qiukan97@gmail.com>
2020-07-27 18:41:08 -0400

    Create DivideTwoIntegers29.java

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/DivideTwoIntegers29.java
@@ -0,0 +1,55 @@
+package leetcode.Mathmatics数学题;
+/**
+ * Description: TODO
+ * Package Name : leetcode.Mathmatics数学题;
+ * File name : DivideTwoIntegers29;
+ * Creator: Kane;
+ * Date: 7/27/20
+ */
+
+/**
+ * +,-
+ * 越界
+ * = 0
+ * Time complexity:O(logn * logn);
+ * Space complexity: O(logn);
+ */
+public class DivideTwoIntegers29 {
+    public static int divide(int divided, int divisor) {
+        int sign = 1;
+        if ((divided > 0 && divisor < 0) || (divided < 0 && divisor > 0)) {
+            sign = -1;
+        }
+        long Ldivided = Math.abs((long) divided);
+        long Ldivisor = Math.abs((long) divisor);
+        if (Ldivided < Ldivisor || Ldivided == 0) {
+            return 0;
+        }
+        long Lres = divide(Ldivided, Ldivisor);
+        int res = 0;
+        if (res > Integer.MAX_VALUE) {
+            res = (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        } else {
+            res = (int)(sign * Lres);
+        }
+        return res;
+    }
+    public static  long divide(long ldivided, long ldivisor) {
+        if (ldivided < ldivisor) {
+            return 0;
+        }
+        long sum = ldivisor;
+        long multiple = 1;
+        while ((sum + sum) <= ldivided) {
+            sum += sum;
+            multiple += multiple;
+        }
+        return multiple + divide(ldivided - sum, ldivisor);
+    }
+
+    public  static void main(String[] args) {
+        System.out.println(divide(1000,2));
+        System.out.println(Integer.MIN_VALUE);
+
+    }
+}
