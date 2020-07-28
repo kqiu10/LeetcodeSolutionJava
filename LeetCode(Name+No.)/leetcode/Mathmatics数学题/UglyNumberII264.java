e40892ec137543f721094802673b2bc3b80c88e9
kqiu10 <qiukan97@gmail.com>
2020-07-28 14:57:58 -0400

    Create UglyNumberII264.java

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/UglyNumberII264.java
@@ -0,0 +1,32 @@
+package leetcode.Mathmatics数学题;
+/**
+ * Package Name : leetcode.Mathmatics数学题;
+ * File name : UglyNumberII264;
+ * Creator: Kane;
+ * Date: 7/28/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class UglyNumberII264 {
+    public static int nthUglyNumber(int n) {
+        int[] nums = new int[n];
+        int index2 = 0, index3 = 0, index5 = 0;
+        nums[0] = 1;
+        for (int i = 1; i < n;i++) {
+            nums[i] = Math.min(nums[index2] * 2, Math.min(nums[index3] * 3, nums[index5] * 5));
+            if (nums[i] == nums[index2] * 2) index2++;
+            if (nums[i] == nums[index3] * 3) index3++;
+            if (nums[i] == nums[index5] * 5) index5++;
+        }
+        return nums[n-1];
+
+    }
+
+    public static void main(String[] args) {
+        nthUglyNumber(10);
+    }
+}
