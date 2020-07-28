b57fb95c03654d77ab0a51d86a27ae4e9470abe7
kqiu10 <qiukan97@gmail.com>
2020-07-28 14:12:18 -0400

    finish 412

--- LeetCode(Name+No.)/leetcode/Mathmatics数学题/FactorialTrailingZeros172.java
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/FactorialTrailingZeros172.java
@@ -1,6 +1,5 @@
 package leetcode.Mathmatics数学题;
 /**
- * Description: TODO
  * Package Name : leetcode.Mathmatics数学题;
  * File name : FactorialTrailingZeros;
  * Creator: Kane;
@@ -8,8 +7,13 @@ package leetcode.Mathmatics数学题;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
+ * Time complexity:O(logn);
+ * Space complexity: O(n);
+ * Description: 2 * 5 = 10
+ * 5出现的次数远小于2出现的次数，所以只需要求得5出现的次数即可
  */
 public class FactorialTrailingZeros172 {
+    public int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
 }
