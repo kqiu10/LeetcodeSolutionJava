16ece892c38429b3629d717f61f702a21d736678
kqiu10 <qiukan97@gmail.com>
2020-07-28 13:44:08 -0400

    finish 507

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/FactorialTrailingZeros172.java
@@ -0,0 +1,15 @@
+package leetcode.Mathmatics数学题;
+/**
+ * Description: TODO
+ * Package Name : leetcode.Mathmatics数学题;
+ * File name : FactorialTrailingZeros;
+ * Creator: Kane;
+ * Date: 7/28/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ */
+public class FactorialTrailingZeros172 {
+}
