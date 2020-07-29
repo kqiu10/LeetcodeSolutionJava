547a2a1753614924312c4fcca48e5f4296342a8b
kqiu10 <qiukan97@gmail.com>
2020-07-29 02:10:45 -0400

    Create FractionToRecurringDecimal166.java

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/FractionToRecurringDecimal166.java
@@ -0,0 +1,48 @@
+package leetcode.Mathmatics数学题;
+/**
+ * Package Name : leetcode.Mathmatics数学题;
+ * File name : FractionToRecurringDecimal166;
+ * Creator: Kane;
+ * Date: 7/28/20
+ */
+
+import java.util.HashMap;
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class FractionToRecurringDecimal166 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder res = new StringBuilder();
+        res.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        res.append(num / den);
+        num %= den;
+        if (num == 0) {
+            return res.toString();
+        }
+        res.append(".");
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(num,res.length());
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
+                break;
+            } else {
+                map.put(num, res.length());
+            }
+        }
+        return res.toString();
+    }
+}
