c9caff1447f0d5206b650115a170bbe89a023049
kqiu10 <qiukan97@gmail.com>
2020-07-29 21:23:29 -0400

    create 493

--- LeetCode(Name+No.)/leetcode/数组/MergeSortedArray88.java
+++ LeetCode(Name+No.)/leetcode/数组/MergeSortedArray88.java
@@ -10,22 +10,16 @@ package leetcode.数组;
 import java.util.Arrays;
 
 /**
- * Best Time complexity: O();
- * Worst Time complexity: O();
- * Average Time complexity:O();
- * Space complexity: O();
+ * Time complexity:O(m + n);
+ * Space complexity: O(1);
  */
 public class MergeSortedArray88 {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
-        while(i>= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
+        while (i >= 0 && j >= 0) {
+            nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
         }
         while (i >= 0) {
             nums1[k--] = nums1[i--];
@@ -36,9 +30,9 @@ public class MergeSortedArray88 {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
-        int[] nums2 = new int[]{2, 5, 6};
-        merge(nums1, 3, nums2 , 3);
+        int[] nums1 = new int[]{0};
+        int[] nums2 = new int[]{1};
+        merge(nums1, 0, nums2 , 1);
         System.out.println(Arrays.toString(nums1));
     }
 }
---
+++ LeetCode(Name+No.)/leetcode/数组/ReversePairs493.java
@@ -0,0 +1,15 @@
+package leetcode.数组;
+/**
+ * Package Name : leetcode.数组;
+ * File name : ReversePairs493;
+ * Creator: Kane;
+ * Date: 7/29/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class ReversePairs493 {
+}
