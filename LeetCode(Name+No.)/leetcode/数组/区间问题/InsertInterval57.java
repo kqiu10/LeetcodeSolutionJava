38d5bceac95af6eb17974f2a3f395675969aa61c
kqiu10 <qiukan97@gmail.com>
2020-08-01 01:37:19 -0400

    Create InsertInterval57.java

---
+++ LeetCode(Name+No.)/leetcode/数组/区间问题/InsertInterval57.java
@@ -0,0 +1,43 @@
+package 数组.区间问题;
+/**
+ * Package Name : 数组.区间问题;
+ * File name : InsertInterval57;
+ * Creator: Kane;
+ * Date: 8/1/20
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class InsertInterval57 {
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        if (intervals == null) return intervals;
+        int len = intervals.length;
+        ArrayList<int[]> res = new ArrayList<>();
+        int i = 0;
+        while (i < len && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i++]);
+        }
+        while (i < len && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+        while (i < len && intervals[i][0] > newInterval[1]) {
+            res.add(intervals[i]);
+            i++;
+        }
+        return res.toArray(new int[res.size()][2]);
+    }
+
+    public static void main(String[] args) {
+        int[][] ls = new int[][]{{1,2},{3,5},{6,7},{8,9},{10,16,17}};
+        int[] insert = new int[]{4,8};
+        System.out.println(insert(ls,insert));
+    }
+}
