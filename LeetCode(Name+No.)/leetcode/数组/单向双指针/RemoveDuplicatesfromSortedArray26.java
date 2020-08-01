7e3e128d0850c052135ca53abc4d216df54b3933
kqiu10 <qiukan97@gmail.com>
2020-08-01 00:32:50 -0400

    finish 80

--- LeetCode(Name+No.)/leetcode/数组/单向双指针/RemoveDuplicatesfromSortedArray26.java
+++ LeetCode(Name+No.)/leetcode/数组/单向双指针/RemoveDuplicatesfromSortedArray26.java
@@ -6,24 +6,27 @@ package 数组.单向双指针;
  * Date: 7/31/20
  */
 
+import java.util.Arrays;
+
 /**
- * Time complexity:O();
- * Space complexity: O();
- * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: len之前的保留数，len所处的属于待定数。
  */
 public class RemoveDuplicatesfromSortedArray26 {
     public  static int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int len = 1;
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i] != nums[i-1]) {
+            if(nums[i] != nums[len-1]) {
                 nums[len++] = nums[i];
             }
         }
+        System.out.println("nums is " + Arrays.toString(nums));
         return len;
     }
 
     public static void main(String[] args) {
-        removeDuplicates(new int[]{1, 1, 2});
+        removeDuplicates(new int[]{1, 1, 1, 1, 1, 1, 2});
     }
 }
