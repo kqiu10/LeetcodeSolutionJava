e8206a4c440582eb26c1cdd2a6f6cc50360cdd83
kqiu10 <qiukan97@gmail.com>
2020-08-02 18:41:32 -0400

    Create DataStreamasDisjointIntervals352.java

--- LeetCode(Name+No.)/leetcode/数组/TreeMap/DataStreamasDisjointIntervals352.java
+++ LeetCode(Name+No.)/leetcode/数组/TreeMap/DataStreamasDisjointIntervals352.java
@@ -1,10 +1,12 @@
 package 数组.TreeMap;
 /**
- * Package Name : 数组.TreeMap;
+ * Package Name : 数组.子数组;
  * File name : DataStreamasDisjointIntervals352;
  * Creator: Kane;
- * Date: 8/1/20
+ * Date: 8/2/20
  */
+
+import java.util.*;
 
 /**
  * Time complexity:O();
@@ -12,4 +14,39 @@ package 数组.TreeMap;
  * Description: TODO
  */
 public class DataStreamasDisjointIntervals352 {
+    TreeMap<Integer, int[]> treemap;
+
+    /** Initialize your data structure here. */
+    public DataStreamasDisjointIntervals352() {
+        treemap = new TreeMap<>();
+    }
+
+    public void addNum(int val) {
+        if (treemap.containsKey(val)) return;
+        Integer lowerKey = treemap.lowerKey(val);
+        Integer higherKey = treemap.higherKey(val);
+        if (lowerKey != null && higherKey != null && (val == treemap.get(lowerKey)[1] + 1 ) && (val + 1 == treemap.get(higherKey)[0])) {
+            treemap.get(lowerKey)[1] = treemap.get(higherKey)[1];
+            treemap.remove(higherKey);
+        } else if (lowerKey != null && (val <= treemap.get(lowerKey)[1] + 1)) {
+                treemap.get(lowerKey)[1] = Math.max(val, treemap.get(lowerKey)[1]);
+            } else if (higherKey != null && (val + 1 == treemap.get(higherKey)[0])) {
+                treemap.put(val,new int[]{val, treemap.get(higherKey)[1]});
+                treemap.remove(higherKey);
+            }else {
+                treemap.put(val, new int[]{val, val});
+            }
+    }
+
+    public int[][] getIntervals() {
+        return treemap.values().toArray(new int[0][0]);
+
+    }
+
+    public static void main(String[] args) {
+        DataStreamasDisjointIntervals352 test = new DataStreamasDisjointIntervals352();
+        test.addNum(4);;
+        System.out.println(test.getIntervals());
+    }
 }
+
