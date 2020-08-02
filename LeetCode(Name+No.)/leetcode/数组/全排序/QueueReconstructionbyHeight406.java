8fe7e589964f55aec6153b4aecaee6f8fe488d29
kqiu10 <qiukan97@gmail.com>
2020-08-01 21:58:23 -0400

    Update QueueReconstructionbyHeight406.java

--- LeetCode(Name+No.)/leetcode/数组/全排序/QueueReconstructionbyHeight406.java
+++ LeetCode(Name+No.)/leetcode/数组/全排序/QueueReconstructionbyHeight406.java
@@ -6,10 +6,23 @@ package 数组.全排序;
  * Date: 8/1/20
  */
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Time complexity:O();
  * Space complexity: O();
  * Description: TODO
  */
 public class QueueReconstructionbyHeight406 {
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length <=1) return people;
+        Arrays.sort(people, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> res = new LinkedList<>();
+        for (int[] cur : people) {
+            res.add(cur[1], cur);
+        }
+        return res.toArray(new int[people.length][]);
+    }
 }
