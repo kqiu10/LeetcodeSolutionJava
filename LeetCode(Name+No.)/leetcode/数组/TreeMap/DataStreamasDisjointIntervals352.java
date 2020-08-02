f570dbf15ba2d591e512d619748c2d1fb0e8a24e
kqiu10 <qiukan97@gmail.com>
2020-08-01 21:58:03 -0400

    finish 406

---
+++ LeetCode(Name+No.)/leetcode/数组/TreeMap/DataStreamasDisjointIntervals352.java
@@ -0,0 +1,15 @@
+package 数组.TreeMap;
+/**
+ * Package Name : 数组.TreeMap;
+ * File name : DataStreamasDisjointIntervals352;
+ * Creator: Kane;
+ * Date: 8/1/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class DataStreamasDisjointIntervals352 {
+}
---
+++ LeetCode(Name+No.)/leetcode/数组/全排序/QueueReconstructionbyHeight406.java
@@ -0,0 +1,15 @@
+package 数组.全排序;
+/**
+ * Package Name : 数组.全排序;
+ * File name : QueueReconstructionbyHeight406;
+ * Creator: Kane;
+ * Date: 8/1/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class QueueReconstructionbyHeight406 {
+}
