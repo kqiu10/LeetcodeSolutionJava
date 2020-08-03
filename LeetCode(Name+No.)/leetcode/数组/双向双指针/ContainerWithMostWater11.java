a8e75127a6ac70ad95aa681d48f7a44ac90c70d9
kqiu10 <qiukan97@gmail.com>
2020-08-03 00:40:31 -0400

    Update ContainerWithMostWater11.java

--- LeetCode(Name+No.)/leetcode/数组/双向双指针/ContainerWithMostWater11.java
+++ LeetCode(Name+No.)/leetcode/数组/双向双指针/ContainerWithMostWater11.java
@@ -13,6 +13,18 @@ package 数组.双向双指针;
  */
 public class ContainerWithMostWater11 {
     public int maxArea(int[] height) {
-
+        if (height.length <= 1) return 0;
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
     }
 }
