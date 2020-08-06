25f1446851d7f45e5db833eb13dc72d5aed4f36e
kqiu10 <qiukan97@gmail.com>
2020-08-06 13:35:29 -0400

    Update RotateArray189.java

--- LeetCode(Name+No.)/leetcode/数组/交换/RotateArray189.java
+++ LeetCode(Name+No.)/leetcode/数组/交换/RotateArray189.java
@@ -7,9 +7,9 @@ package 数组.交换;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
- * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Description: Using Extra Array
  */
 public class RotateArray189 {
     public void rotate(int[] nums, int k) {
@@ -21,5 +21,33 @@ public class RotateArray189 {
             nums[i] = temp[i];
         }
 
+    }
+}
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: Using Reverse
+ * Original List                   : 1 2 3 4 5 6 7
+ * After reversing all numbers     : 7 6 5 4 3 2 1
+ * After reversing first k numbers : 5 6 7 4 3 2 1
+ * After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+ */
+class RotateArrayII{
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp =nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
