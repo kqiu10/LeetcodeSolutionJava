fbc6177aef12e43cfc9971166e589b21aedae88e
kqiu10 <qiukan97@gmail.com>
2020-08-05 21:35:13 -0400

    Update TrappingRainWater42.java

--- LeetCode(Name+No.)/leetcode/数组/双向双指针/TrappingRainWater42.java
+++ LeetCode(Name+No.)/leetcode/数组/双向双指针/TrappingRainWater42.java
@@ -7,12 +7,28 @@ package 数组.双向双指针;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
+ * Time complexity:O(n);
+ * Space complexity: O(1);
  * Description: TODO
  */
 public class TrappingRainWater42 {
     public int trap(int[] height) {
-
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
     }
 }
