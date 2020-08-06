ab487793a01a1f622104bfa55f740f0ff50864a8
kqiu10 <qiukan97@gmail.com>
2020-08-05 22:56:20 -0400

    Create ThreeSummaller259.java

---
+++ LeetCode(Name+No.)/leetcode/数组/三指针/ThreeSummaller259.java
@@ -0,0 +1,34 @@
+package 数组.三指针;
+/**
+ * Package Name : 数组.三指针;
+ * File name : ThreeSummaller259;
+ * Creator: Kane;
+ * Date: 8/5/20
+ */
+
+import java.util.Arrays;
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class ThreeSummaller259 {
+    public int threeSumSmaller(int[] nums, int target) {
+        int res = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < target) {
+                    res += r - l;
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
