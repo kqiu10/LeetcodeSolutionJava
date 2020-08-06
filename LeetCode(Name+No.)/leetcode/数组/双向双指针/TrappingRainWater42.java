e2f0a7bdd56407fce2885162a52509dc24582e9a
kqiu10 <qiukan97@gmail.com>
2020-08-05 21:14:59 -0400

    Create TrappingRainWater42.java

---
+++ LeetCode(Name+No.)/leetcode/数组/双向双指针/TrappingRainWater42.java
@@ -0,0 +1,18 @@
+package 数组.双向双指针;
+/**
+ * Package Name : 数组.双向双指针;
+ * File name : TrappingRainWater42;
+ * Creator: Kane;
+ * Date: 8/3/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class TrappingRainWater42 {
+    public int trap(int[] height) {
+
+    }
+}
