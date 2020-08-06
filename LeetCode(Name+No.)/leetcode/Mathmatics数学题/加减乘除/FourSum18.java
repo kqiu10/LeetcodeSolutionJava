5b7b006bf2fa184b891c4538ac65bdf768bf8068
kqiu10 <qiukan97@gmail.com>
2020-08-05 23:17:44 -0400

    Create FourSum18.java

---
+++ LeetCode(Name+No.)/leetcode/Mathmatics数学题/加减乘除/FourSum18.java
@@ -0,0 +1,48 @@
+package Mathmatics数学题.加减乘除;
+/**
+ * Package Name : Mathmatics数学题.加减乘除;
+ * File name : FourSum18;
+ * Creator: Kane;
+ * Date: 8/5/20
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Time complexity:O(n^3);
+ * Space complexity: O(n^2);
+ * Description: TODO
+ */
+public class FourSum18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 4) return res;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    } else if (sum < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+
+        }
+        return res;
+    }
+}
