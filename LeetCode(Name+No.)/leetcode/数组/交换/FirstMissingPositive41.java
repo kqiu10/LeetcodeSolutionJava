cd9610374507535c0a91329f7f769dcdaa96cd5b
kqiu10 <qiukan97@gmail.com>
2020-08-07 00:46:37 -0400

    Update FirstMissingPositive41.java

--- LeetCode(Name+No.)/leetcode/数组/交换/FirstMissingPositive41.java
+++ LeetCode(Name+No.)/leetcode/数组/交换/FirstMissingPositive41.java
@@ -7,9 +7,30 @@ package 数组.交换;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
+ * Time complexity:O(n);
+ * Space complexity: O(1);
  * Description: TODO
  */
 public class FirstMissingPositive41 {
+    public static int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 1;
+        int res = 1;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int temp =  nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    public static void main(String[] args) {
+        firstMissingPositive(new int[]{3, 4, -1, 1});
+    }
 }
