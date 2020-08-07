227ae89f36da43b3da525bd99a9a60dc2da0034a
kqiu10 <qiukan97@gmail.com>
2020-08-07 01:51:25 -0400

    Create DistributeCandies575.java

---
+++ LeetCode(Name+No.)/leetcode/数组/HashSet/DistributeCandies575.java
@@ -0,0 +1,24 @@
+package 数组.HashSet;
+/**
+ * Package Name : 数组.HashSet;
+ * File name : DistributeCandies575;
+ * Creator: Kane;
+ * Date: 8/7/20
+ */
+
+import java.util.HashSet;
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class DistributeCandies575 {
+    public int distributeCandies(int[] candies) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int candy : candies) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candies.length / 2);
+    }
+}
