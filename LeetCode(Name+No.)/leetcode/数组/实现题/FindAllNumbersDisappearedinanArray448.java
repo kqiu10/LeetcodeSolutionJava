8455e5a7e5edea272bdab4070b80cfc641521fe5
kqiu10 <qiukan97@gmail.com>
2020-08-08 18:04:08 -0400

    Create FindAllNumbersDisappearedinanArray448.java

---
+++ LeetCode(Name+No.)/leetcode/数组/实现题/FindAllNumbersDisappearedinanArray448.java
@@ -0,0 +1,64 @@
+package 数组.实现题;
+/**
+ * Package Name : 数组.实现题;
+ * File name : FindAllNumbersDisappearedinanArray448;
+ * Creator: Kane;
+ * Date: 8/8/20
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: swap
+ */
+public class FindAllNumbersDisappearedinanArray448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (i > 0 && nums[nums[i] - 1 ] != nums[i]) {
+                int temp =  nums[nums[i] - 1 ];
+                nums[nums[i] - 1 ] = nums[i];
+                nums[i] = temp;
+            }
+            if (nums[nums[i] - 1] == nums[i]) {
+                continue;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 1 != i) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+
+
+    }
+}
+
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: Like BucketSort
+ */
+class FindAllNumbersDisappearedinanArrayII{
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = - nums[index];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}
