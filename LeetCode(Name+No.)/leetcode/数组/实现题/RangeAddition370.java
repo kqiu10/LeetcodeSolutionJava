d585ef9b1dfcbb4bdf3452ad533484183a291c75
kqiu10 <qiukan97@gmail.com>
2020-08-09 01:50:09 -0400

    Create RangeAddition370.java

---
+++ LeetCode(Name+No.)/leetcode/数组/实现题/RangeAddition370.java
@@ -0,0 +1,32 @@
+package 数组.实现题;
+/**
+ * Package Name : 数组.实现题;
+ * File name : RangeAddition370;
+ * Creator: Kane;
+ * Date: 8/9/20
+ */
+
+/**
+ * Time complexity:O(k + n);
+ * Space complexity: O(n);
+ * Description: 时间复杂度说明：先标记k次， 然后做叠加，所以O(k + n)
+ */
+public class RangeAddition370 {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] res = new int[length];
+        for (int[] update : updates) {
+            int value = update[2];
+            int start = update[0];
+            int end = update[1];
+            res[start] += value;
+            if (end + 1 < length) {
+                res[end + 1] -= value;
+            }
+        }
+        for (int i = 1; i < length; i++) {
+            res[i] += res[i - 1];
+
+        }
+        return res;
+    }
+}
