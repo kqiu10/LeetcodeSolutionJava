effcd271398b9ef3b4099e4faade053761175dcb
kqiu10 <qiukan97@gmail.com>
2020-08-12 17:08:14 -0400

    create 363

---
+++ LeetCode(Name+No.)/leetcode/数组/TreeSet/MaxSumofRectangleNoLargerThanK363.java
@@ -0,0 +1,15 @@
+package 数组.TreeSet;
+/**
+ * Package Name : 数组.TreeSet;
+ * File name : MaxSumofRectangleNoLargerThanK363;
+ * Creator: Kane;
+ * Date: 8/12/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class MaxSumofRectangleNoLargerThanK363 {
+}
---
+++ LeetCode(Name+No.)/leetcode/数组/特殊/ValidSudoku36.java
@@ -0,0 +1,15 @@
+package 数组.特殊;
+/**
+ * Package Name : 数组.特殊;
+ * File name : ValidSudoku36;
+ * Creator: Kane;
+ * Date: 8/12/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class ValidSudoku36 {
+}
