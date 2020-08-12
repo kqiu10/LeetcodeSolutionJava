33d3f74703089ad31c51e473ba7acce94cb6cbbb
kqiu10 <qiukan97@gmail.com>
2020-08-11 22:48:57 -0400

    Create ReshapetheMatrix566.java

---
+++ LeetCode(Name+No.)/leetcode/数组/二维数组/ReshapetheMatrix566.java
@@ -0,0 +1,35 @@
+package 数组.二维数组;
+/**
+ * Package Name : 数组.二维数组;
+ * File name : ReshapetheMatrix566;
+ * Creator: Kane;
+ * Date: 8/11/20
+ */
+
+/**
+ * Time complexity:O(m * n);
+ * Space complexity: O(r * c);
+ * Description:
+ * reshape a matrix, the row index 0f new element is value of element / column, the column index is value of element % column;
+ * Ex 0 1 2
+ *    3 4 5
+ *    the location of 5 is (5 / 3， 5 % 3） == （1 , 2)
+ */
+public class ReshapetheMatrix566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != m * n) return nums;
+        int idx = 0;
+        for (int[] row : nums) {
+            for (int num : row) {
+                res[idx / c][idx % c] = num;
+                idx++;
+            }
+
+        }
+        return res;
+
+    }
+}
