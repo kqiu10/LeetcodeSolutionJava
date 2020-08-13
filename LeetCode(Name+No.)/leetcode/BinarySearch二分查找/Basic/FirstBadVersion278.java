45676244cf4488d742c6b833131e9ee3c738f84a
kqiu10 <qiukan97@gmail.com>
2020-08-12 23:58:58 -0400

    Update FirstBadVersion278.java

--- LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic/FirstBadVersion278.java
+++ LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic/FirstBadVersion278.java
@@ -15,7 +15,7 @@ public class FirstBadVersion278 {
     public int firstBadVersion(int n) {
         int l = 1;
         int r = n;
-        while (l <= r) {
+        while (l < r) {
             int mid = (r - l) / 2 + l;
             if (isBadVersion(mid)) r = mid - 1;
             else l = mid + 1;
