3f096df59cddb9054654799f11c01a5620d4953b
kqiu10 <qiukan97@gmail.com>
2020-08-12 21:27:42 -0400

    Create SearchInsertPosition35.java

---
+++ LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic/SearchInsertPosition35.java
@@ -0,0 +1,15 @@
+package BinarySearch二分查找.Basic;
+/**
+ * Package Name : BinarySearch二分查找.基础;
+ * File name : SearchInsertPosition35;
+ * Creator: Kane;
+ * Date: 8/12/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class SearchInsertPosition35 {
+}
