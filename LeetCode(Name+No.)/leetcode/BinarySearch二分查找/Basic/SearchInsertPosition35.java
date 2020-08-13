b5a5435b9398bda5148dbca1253502a07ad63faa
kqiu10 <qiukan97@gmail.com>
2020-08-12 21:37:57 -0400

    Update SearchInsertPosition35.java

--- LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic/SearchInsertPosition35.java
+++ LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic/SearchInsertPosition35.java
@@ -7,9 +7,22 @@ package BinarySearch二分查找.Basic;
  */
 
 /**
- * Time complexity:O();
- * Space complexity: O();
+ * Time complexity:O(logn);
+ * Space complexity: O(1);
  * Description: TODO
  */
 public class SearchInsertPosition35 {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1; ;
+        int pos = 0;
+        while (l <= r) {
+            pos = (r - l) / 2 + l;
+            if (nums[pos] > target) r = pos - 1;
+            else if (nums[pos] < target) l = pos + 1;
+            else return pos;
+        }
+        return l;
+
+    }
 }
