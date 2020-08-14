419359746e6f436907adb562930a7d927875c0f9
kqiu10 <qiukan97@gmail.com>
2020-08-14 19:19:38 -0400

    Create HIndexII275.java

---
+++ LeetCode(Name+No.)/leetcode/BinarySearch二分查找/Basic基础/HIndexII275.java
@@ -0,0 +1,30 @@
+package BinarySearch二分查找.Basic基础;
+/**
+ * Package Name : BinarySearch二分查找.Basic基础;
+ * File name : HIndexII275;
+ * Creator: Kane;
+ * Date: 8/14/20
+ */
+
+/**
+ * Time complexity:O(logn);
+ * Space complexity: O(1);
+ * Description: TODO
+ */
+public class HIndexII275 {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int start = 0;
+        int end = citations.length - 1;
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (citations[mid] > len - mid) end = mid - 1;
+            else if (citations[mid] < len - mid) start = mid + 1;
+            else return len - mid;
+
+        }
+        return len - start;
+
+
+    }
+}
