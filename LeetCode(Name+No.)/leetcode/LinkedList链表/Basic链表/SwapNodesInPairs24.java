ab3286f7b18d8a12ef54f0a59a0fa99fd1cdd4c1
kqiu10 <qiukan97@gmail.com>
2020-08-16 01:56:43 -0400

    Create SwapNodesInPairs24.java

---
+++ LeetCode(Name+No.)/leetcode/LinkedList链表/Basic链表/SwapNodesInPairs24.java
@@ -0,0 +1,49 @@
+package LinkedList链表.Basic链表;
+/**
+ * Package Name : LinkedList链表.Basic链表;
+ * File name : SwapNodesInPairs24;
+ * Creator: Kane;
+ * Date: 8/16/20
+ */
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: TODO
+ */
+public class SwapNodesInPairs24 {
+     class ListNode {
+         int val;
+         ListNode next;
+
+         ListNode() {
+         }
+
+         ListNode(int val) {
+             this.val = val;
+         }
+
+         ListNode(int val, ListNode next) {
+             this.val = val;
+             this.next = next;
+         }
+     }
+     public ListNode swapPairs(ListNode head) {
+         if (head == null || head.next == null) return head;
+         ListNode dummy = new ListNode(0);
+         ListNode l1 = dummy;
+         ListNode l2 = head;
+         while (l2 != null && l2.next != null) {
+             ListNode nextStart = l2.next.next;
+             l1.next = l2.next;
+             l2.next.next = l2;
+             l2.next = nextStart;
+             l1 = l2;
+             l2 = l2.next;
+         }
+         return dummy.next;
+
+
+     }
+
+}
