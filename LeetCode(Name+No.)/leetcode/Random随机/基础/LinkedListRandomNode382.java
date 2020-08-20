8a8c7a1cae96e808036fa93ab126687e58c2a3b0
kqiu10 <qiukan97@gmail.com>
2020-08-20 05:42:17 -0400

    finsih 398

---
+++ LeetCode(Name+No.)/leetcode/Random随机/基础/LinkedListRandomNode382.java
@@ -0,0 +1,15 @@
+package Random随机.基础;
+/**
+ * Package Name : Random随机.基础;
+ * File name : LinkedListRandomNode382;
+ * Creator: Kane;
+ * Date: 8/20/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class LinkedListRandomNode382 {
+}
