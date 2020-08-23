9de9f887e8a1047167953208946eeb1695cd3a22
kqiu10 <qiukan97@gmail.com>
2020-08-23 19:54:07 -0400

    Update FractionToRecurringDecimal166.java

--- LeetCode(Name+No.)/leetcode/Mathematics数学题/规律/FractionToRecurringDecimal166.java
+++ LeetCode(Name+No.)/leetcode/Mathematics数学题/规律/FractionToRecurringDecimal166.java
@@ -17,35 +17,38 @@ import java.util.HashMap;
  * 3. 小数 -> 循环
  */
 public class FractionToRecurringDecimal166 {
-    public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0) {
-            return "0";
-        }
-        StringBuilder res = new StringBuilder();
-        res.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+    public static String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        sb.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
         long num = Math.abs((long)numerator);
         long den = Math.abs((long)denominator);
-        res.append(num / den);
+        sb.append(num / den);
         num %= den;
         if (num == 0) {
-            return res.toString();
+            return sb.toString();
         }
-        res.append(".");
+        sb.append(".");
         HashMap<Long, Integer> map = new HashMap<>();
-        map.put(num,res.length());
+        map.put(num, sb.length());
         while (num != 0) {
             num *= 10;
-            res.append(num / den);
+            sb.append(num / den);
             num %= den;
             if (map.containsKey(num)) {
                 int index = map.get(num);
-                res.insert(index, "(");
-                res.append(")");
+                sb.insert(index, "(");
+                sb.append(")");
                 break;
             } else {
-                map.put(num, res.length());
+                map.put(num, sb.length());
             }
         }
-        return res.toString();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        fractionToDecimal(-1,
+                -2147483648);
     }
 }
