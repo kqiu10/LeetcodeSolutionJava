e4ebfd52981981c74be3dbcc46623119da34a2e8
kqiu10 <qiukan97@gmail.com>
2020-08-26 17:03:45 -0400

    Update _385_MiniParser.java

--- LeetCode(Name+No.)/leetcode/Stack栈/压栈匹配/_385_MiniParser.java
+++ LeetCode(Name+No.)/leetcode/Stack栈/压栈匹配/_385_MiniParser.java
@@ -78,34 +78,34 @@ import java.util.Stack;
  */
 
 public class _385_MiniParser {
-    public NestedInteger deserialize(String s) {
-        if (!s.startsWith("[")) {
-            return new NestedInteger(Integer.valueOf(s)) {
-        }
-        Stack<NestedInteger> stack = new Stack<>();
-        NestedInteger res = new NestedInteger();
-        stack.push(res);
-        int start = 1;
-        for (int i = 1; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '[') {
-                NestedInteger nestedInteger = new NestedInteger();
-                stack.peek().add(nestedInteger);
-                stack.push(nestedInteger);
-                start = i + 1;
-            } else if (c == ',' || c == ']') {
-                if (i > start) {
-                    Integer val = Integer.valueOf(s.substring(start, i));
-                    stack.peek().add(new NestedInteger(val));
-                }
-                start = i + 1;
-                if (c == ']') {
-                    stack.pop();
-                }
-
-            }
-        }
-        return res;
+    public void deserialize(String s) {
+//        if (!s.startsWith("[")) {
+//            return new NestInteger(Integer.valueOf(s)) {
+//        }
+//        Stack<NestedInteger> stack = new Stack<>();
+//        NestedInteger res = new NestedInteger();
+//        stack.push(res);
+//        int start = 1;
+//        for (int i = 1; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if (c == '[') {
+//                NestedInteger nestedInteger = new NesdInteger();
+//                stack.peek().add(nestedInteger);
+//                stack.push(nestedInteger);
+//                start = i + 1;
+//            } else if (c == ',' || c == ']') {
+//                if (i > start) {
+//                    Integer val = Integer.valueOf(s.substring(start, i));
+//                    stack.peek().add(new NestInteger(val));
+//                }
+//                start = i + 1;
+//                if (c == ']') {
+//                    stack.pop();
+//                }
+//
+//            }
+//        }
+//        return res;
     }
 }
 
