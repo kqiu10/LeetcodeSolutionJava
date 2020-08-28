4dfcf4466e6e3d2dfdae62b5b9a83aac4a89ed3d
kqiu10 <qiukan97@gmail.com>
2020-08-28 00:33:23 -0400

    update 161

---
+++ LeetCode(Name+No.)/leetcode/String字符串/base基础/_434_NumberofSegmentsinaString.java
@@ -0,0 +1,15 @@
+package String字符串.base基础;
+/**
+ * Package Name : String字符串.base基础;
+ * File name : _434_NumberofSegmentsinaString;
+ * Creator: Kane;
+ * Date: 8/28/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _434_NumberofSegmentsinaString {
+}
