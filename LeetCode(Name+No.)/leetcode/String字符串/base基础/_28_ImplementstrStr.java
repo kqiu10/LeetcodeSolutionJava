bc23fd11743df047cd31a500ecb773a119afb916
kqiu10 <qiukan97@gmail.com>
2020-08-27 22:15:27 -0400

    Create _28_ImplementstrStr.java

---
+++ LeetCode(Name+No.)/leetcode/String字符串/base基础/_28_ImplementstrStr.java
@@ -0,0 +1,25 @@
+package String字符串.base基础;
+/**
+ * Package Name : String字符串.base基础;
+ * File name : _28_ImplementstrStr;
+ * Creator: Kane;
+ * Date: 8/27/20
+ */
+
+/**
+ * Time complexity:O(k * (m - k));
+ * Space complexity: O(1);
+ * Description: TODO
+ */
+public class _28_ImplementstrStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length()) return -1;
+        int k = needle.length(), m = haystack.length();
+        for (int i = 0; i < m - k; i++) {
+            if (haystack.substring(i, k + i).equals(needle)) return i;
+        }
+        return -1;
+
+    }
+}
+}
