2e7e653e7e572ecd38767b91f1ade8c8a014b7b9
kqiu10 <qiukan97@gmail.com>
2020-08-28 00:12:11 -0400

    Create _171_ExcelSheetColumnNumber.java

---
+++ LeetCode(Name+No.)/leetcode/String字符串/base基础/_171_ExcelSheetColumnNumber.java
@@ -0,0 +1,47 @@
+package String字符串.base基础;
+/**
+ * Package Name : String字符串.base基础;
+ * File name : _171_ExcelSheetColumnNumber;
+ * Creator: Kane;
+ * Date: 8/27/20
+ */
+
+/**
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ *
+ * For example:
+ *
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ * Example 1:
+ *
+ * Input: "A"
+ * Output: 1
+ * Example 2:
+ *
+ * Input: "AB"
+ * Output: 28
+ * Example 3:
+ *
+ * Input: "ZY"
+ * Output: 701
+ *
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: TODO
+ */
+public class _171_ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res = res * 26 +(s.charAt(i) - 'A' + 1);
+        }
+        return res;
+    }
+}
