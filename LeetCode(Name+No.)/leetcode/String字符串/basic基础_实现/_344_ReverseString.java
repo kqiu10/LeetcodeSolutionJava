88afb7367966d06aea7149db61a5ee13e96b3b81
kqiu10 <qiukan97@gmail.com>
2020-08-28 13:07:53 -0400

    Create _344_ReverseString.java

---
+++ LeetCode(Name+No.)/leetcode/String字符串/basic基础_实现/_344_ReverseString.java
@@ -0,0 +1,26 @@
+package String字符串.basic基础_实现;
+/**
+ * Package Name : String字符串.basic基础_实现;
+ * File name : _344_ReverseString;
+ * Creator: Kane;
+ * Date: 8/28/20
+ */
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: TODO
+ */
+public class _344_ReverseString {
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) return;
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
