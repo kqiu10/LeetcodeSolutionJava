338f25fdde96c81a394285c675825d30267fb4b4
kqiu10 <qiukan97@gmail.com>
2020-08-28 22:48:39 -0400

    finish 557

---
+++ LeetCode(Name+No.)/leetcode/String字符串/basic基础_实现/_556_NextGreaterElementIII.java
@@ -0,0 +1,34 @@
+package String字符串.basic基础_实现;
+/**
+ * Package Name : String字符串.basic基础_实现;
+ * File name : _556_NextGreaterElementIII;
+ * Creator: Kane;
+ * Date: 8/28/20
+ */
+
+/**
+ * Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has
+ * exactly the same digits existing in the integer n and is greater in value than n. If no such
+ * positive 32-bit integer exists, you need to return -1.
+ *
+ * Example 1:
+ *
+ * Input: 12
+ * Output: 21
+ *
+ *
+ * Example 2:
+ *
+ * Input: 21
+ * Output: -1
+ *
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _556_NextGreaterElementIII {
+    public int nextGreaterElement(int n) {
+        
+
+    }
+}
