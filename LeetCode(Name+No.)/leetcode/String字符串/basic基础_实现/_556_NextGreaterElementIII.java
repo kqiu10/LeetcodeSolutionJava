548d85be2bd061e23f7b02e63a14666cbe7fd63f
kqiu10 <qiukan97@gmail.com>
2020-08-28 23:01:46 -0400

    Update _556_NextGreaterElementIII.java

--- LeetCode(Name+No.)/leetcode/String字符串/basic基础_实现/_556_NextGreaterElementIII.java
+++ LeetCode(Name+No.)/leetcode/String字符串/basic基础_实现/_556_NextGreaterElementIII.java
@@ -22,13 +22,41 @@ package String字符串.basic基础_实现;
  * Input: 21
  * Output: -1
  *
- * Time complexity:O();
- * Space complexity: O();
- * Description: TODO
+ * Time complexity:O(1); cuz it's only 32-bit integer that is calculated
+ * Space complexity: O(1);
+ * Description:
+ * Ex take two steps
+ * 1 2 3 4 7 6 5 5 3 -> 1 2 3 5 7 6 5 4 3 -> 1 3 4 5 3 4 5 6 7
  */
 public class _556_NextGreaterElementIII {
     public int nextGreaterElement(int n) {
-        
+        char[] c = ("" + n).toCharArray();
+        int i = c.length - 2;
+        while (i >= 0 && c[i] >= c[i + 1]) {
+            i--;
 
+        }
+        if (i == -1) return -1;
+        int j = c.length - 1;
+        while (j >= 0 && c[j] <= c[i]) {
+            j--;
+        }
+        swap(c, i, j);
+        reverse(c, i+1, c.length - 1);
+        long val = Long.parseLong(c.toString());
+        return val <= Integer.MAX_VALUE ? (int)val : -1;
+    }
+
+    private void swap(char[] c, int a, int b) {
+        char temp = c[a];
+        c[a] = c[b];
+        c[b] = temp;
+    }
+    private void reverse(char[]c, int a, int b) {
+        while (a < b) {
+            char temp = c[a];
+            c[a++] = c[b];
+            c[b--] = temp;
+        }
     }
 }
