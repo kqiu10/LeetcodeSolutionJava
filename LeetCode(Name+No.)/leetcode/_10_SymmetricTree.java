30b0f19e5a091575e1e8b52f54d4c6459fdfe5f1
kqiu10 <qiukan97@gmail.com>
2020-09-02 16:10:45 -0400

    Update _10_SymmetricTree.java

--- LeetCode(Name+No.)/leetcode/_10_SymmetricTree.java
+++ LeetCode(Name+No.)/leetcode/_10_SymmetricTree.java
@@ -18,7 +18,7 @@ public class _10_SymmetricTree {
     private boolean isMirror(TreeNode n1, TreeNode n2) {
         if (n1 == null && n2 == null) return true;
         if (n1 == null || n2 == null) return false;
-        return (n1.val == n2.val) && isMirror(n1.left, n2.left) && isMirror(n1.right, n2.right);
+        return (n1.val == n2.val) && isMirror(n1.right, n2.left) && isMirror(n1.left, n2.right);
     }
     private class TreeNode {
       int val;
