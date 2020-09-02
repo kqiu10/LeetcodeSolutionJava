38f71a65c0ac2932e26849e8db7adeb3d63fc474
kqiu10 <qiukan97@gmail.com>
2020-09-02 16:10:29 -0400

    finish 101

---
+++ LeetCode(Name+No.)/leetcode/String字符串/实现题/_158_ReadNCharactersGivenRead4IICallmultipletimes.java
@@ -0,0 +1,37 @@
+package String字符串.实现题;
+/**
+ * Package Name : String字符串.实现题;
+ * File name : _158_ReadNCharactersGivenRead4IICallmultipletimes;
+ * Creator: Kane;
+ * Date: 9/1/20
+ */
+
+/**
+ * Given a file and assume that you can only read the file using a given method read4, implement
+ * a method read to read n characters. Your method read may be called multiple times.
+ *
+ *
+ *
+ * Method read4:
+ *
+ * The API read4 reads 4 consecutive characters from the file, then writes those characters into
+ * the buffer array buf.
+ *
+ * The return value is the number of actual characters read.
+ *
+ * Note that read4() has its own file pointer, much like FILE *fp in C.
+ *
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _158_ReadNCharactersGivenRead4IICallmultipletimes {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+
+    }
+}
---
+++ LeetCode(Name+No.)/leetcode/_10_SymmetricTree.java
@@ -0,0 +1,35 @@
+/**
+ * Package Name : PACKAGE_NAME;
+ * File name : _10_SymmetricTree;
+ * Creator: Kane;
+ * Date: 9/2/20
+ */
+
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class _10_SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+    private boolean isMirror(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
+        return (n1.val == n2.val) && isMirror(n1.left, n2.left) && isMirror(n1.right, n2.right);
+    }
+    private class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
