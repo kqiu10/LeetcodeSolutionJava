c9afacd783bb0139296ad97e3f22079474827c14
kqiu10 <qiukan97@gmail.com>
2020-09-03 20:24:40 -0400

    finish 316

---
+++ LeetCode(Name+No.)/leetcode/HashMap&SlidingWindow/HashMap/_535_EncodeandDecodeTinyURL.java
@@ -0,0 +1,15 @@
+package HashMap;
+/**
+ * Package Name : HashMap;
+ * File name : _535_EncodeandDecodeTinyURL;
+ * Creator: Kane;
+ * Date: 9/3/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _535_EncodeandDecodeTinyURL {
+}
