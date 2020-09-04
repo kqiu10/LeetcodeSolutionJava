92060840676c7913bf9210d16347dd1bd545c9f9
kqiu10 <qiukan97@gmail.com>
2020-09-03 21:19:49 -0400

    Create _228_SummaryRanges.java

---
+++ LeetCode(Name+No.)/leetcode/HashMap&SlidingWindow/SlidingWindow/_228_SummaryRanges.java
@@ -0,0 +1,47 @@
+package SlidingWindow;
+/**
+ * Package Name : SlidingWindow;
+ * File name : _228_SummaryRanges;
+ * Creator: Kane;
+ * Date: 9/3/20
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a sorted integer array without duplicates, return the summary of its ranges.
+ *
+ * Example 1:
+ *
+ * Input:  [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+ * Example 2:
+ *
+ * Input:  [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+ *
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class _228_SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if (num != nums[i]) {
+                res.add(num + "->" + nums[i]);
+            } else {
+                res.add(num + "");
+            }
+        }
+        return res;
+    }
+}
