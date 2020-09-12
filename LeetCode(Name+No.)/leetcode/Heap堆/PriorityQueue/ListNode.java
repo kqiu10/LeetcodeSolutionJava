f4b0f51ba5ed938d28ddd07b89dc75fa03db3ef4
kqiu10 <qiukan97@gmail.com>
2020-09-11 22:31:46 -0400

    finish 23

---
+++ LeetCode(Name+No.)/leetcode/Heap堆/PriorityQueue/ListNode.java
@@ -0,0 +1,29 @@
+package Heap堆.PriorityQueue;
+/**
+ * Package Name : LinkedList链表.Basic链表;
+ * File name : ListNode;
+ * Creator: Kane;
+ * Date: 8/16/20
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+}
