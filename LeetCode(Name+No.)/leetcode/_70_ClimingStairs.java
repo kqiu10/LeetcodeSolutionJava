df9fcf0b15fd9ded53308a887dd29ae5e199c14d
kqiu10 <qiukan97@gmail.com>
2020-09-11 23:54:19 -0400

    Create _70_ClimingStairs.java

---
+++ LeetCode(Name+No.)/leetcode/_70_ClimingStairs.java
@@ -0,0 +1,27 @@
+/**
+ * Package Name : PACKAGE_NAME;
+ * File name : _70_ClimingStairs;
+ * Creator: Kane;
+ * Date: 9/11/20
+ */
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+ * Description: Fibonacci Solution
+ */
+public class _70_ClimingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
