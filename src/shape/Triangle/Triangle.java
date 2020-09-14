6b7bf4af1728c0fb2875b7ef77163f7021bb3171
kqiu10 <qiukan97@gmail.com>
2020-09-14 13:47:01 -0400

    Create Triangle.java

---
+++ src/shape/Triangle/Triangle.java
@@ -0,0 +1,34 @@
+package src.shape.Triangle;
+/**
+ * Package Name : src.shape.Triangle;
+ * File name : Triangle;
+ * Creator: Kane;
+ * Date: 9/14/20
+ */
+
+import java.util.Arrays;
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class Triangle {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int start = 0;
+            int end = i - 1;
+            while (start < end) {
+                if (nums[start] + nums[end] <= nums[i]) {
+                    start++;
+                    continue;
+                }
+                res += (end - start);
+                end--;
+            }
+        }
+        return res;
+    }
+}
