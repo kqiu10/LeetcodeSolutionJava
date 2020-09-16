2e7fe49fbc6255f984396262fc2f0fce409d7a69
kqiu10 <qiukan97@gmail.com>
2020-09-16 10:57:13 -0400

    Create _254_FactorCombinations.java

---
+++ leetcode/BackTracking回溯法/组合/_254_FactorCombinations.java
@@ -0,0 +1,81 @@
+package leetcode.BackTracking回溯法.组合;
+/**
+ * Package Name : leetcode.BackTracking回溯法.组合;
+ * File name : _254_FactorCombinations;
+ * Creator: Kane;
+ * Date: 9/16/20
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Numbers can be regarded as product of its factors. For example,
+ *
+ * 8 = 2 x 2 x 2;
+ *   = 2 x 4.
+ * Write a function that takes an integer n and return all possible combinations of its factors.
+ *
+ * Note:
+ *
+ * You may assume that n is always positive.
+ * Factors should be greater than 1 and less than n.
+ * Example 1:
+ *
+ * Input: 1
+ * Output: []
+ * Example 2:
+ *
+ * Input: 37
+ * Output:[]
+ * Example 3:
+ *
+ * Input: 12
+ * Output:
+ * [
+ *   [2, 6],
+ *   [2, 2, 3],
+ *   [3, 4]
+ * ]
+ * Example 4:
+ *
+ * Input: 32
+ * Output:
+ * [
+ *   [2, 16],
+ *   [2, 2, 8],
+ *   [2, 2, 2, 4],
+ *   [2, 2, 2, 2, 2],
+ *   [2, 4, 4],
+ *   [4, 8]
+ * ]
+ *
+ * Time complexity:O(2^n);
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class _254_FactorCombinations {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res, new ArrayList<>(), n, 2);
+        return res;
+
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> list, int n, int start) {
+        if (n == 1) {
+            if (list.size() > 1) {
+                res.add(new ArrayList<>(list));
+                return;
+            }
+        }
+        for (int i = start; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+                helper(res, list, n / i, i);
+                list.remove(list.size() - 1);
+            }
+        }
+
+    }
+}
