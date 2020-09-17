a6480ab3557237f3a84d77925d5b82cfc526c52d
kqiu10 <qiukan97@gmail.com>
2020-09-16 22:30:02 -0400

    Create _200_NumberofIslands.java

---
+++ leetcode/_200_NumberofIslands.java
@@ -0,0 +1,66 @@
+package leetcode;
+/**
+ * Date: 9/16/20
+ * Question Description
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is
+ * surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You
+ * may assume all four edges of the grid are all surrounded by water.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ */
+
+/**
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+ * Description: TODO
+ */
+public class _200_NumberofIslands {
+    public static int numIslands(char[][] grid) {
+        int res = 0;
+        if (grid == null || grid.length == 0) return res;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i , j);
+                }
+            }
+        }
+
+        return res;
+
+    }
+    private static void dfs(char[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        if (i < 0 || j < 0 || i > row || col > row || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i -1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+    public static void main(String[] args) {
+    }
+
+
+}
