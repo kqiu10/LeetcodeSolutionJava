31f6a8513ed8ddc68e78fbdfacf1462dee13155e
kqiu10 <qiukan97@gmail.com>
2020-09-16 22:04:04 -0400

    Create _401_BinaryWatch.java

---
+++ leetcode/BackTracking回溯法/实现题/_401_BinaryWatch.java
@@ -0,0 +1,26 @@
+package leetcode.BackTracking回溯法.实现题;
+/**
+ * Date: 9/16/20
+ * Question Description
+ * A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the
+ * bottom represent the minutes (0-59).
+ *
+ * Each LED represents a zero or one, with the least significant bit on the right.
+ *
+ * Given a non-negative integer n which represents the number of LEDs that are currently on,
+ * return all possible times the watch could represent.
+ *
+ * Example:
+ *
+ * Input: n = 1
+ * Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+ *
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _401_BinaryWatch {
+}
