1e014b4531083c5bc2f3e9d273ee9801d2cf1e7d
kqiu10 <qiukan97@gmail.com>
2020-09-19 19:45:37 -0400

    Update DFS.java

--- src/graph/DFS.java
+++ src/graph/DFS.java
@@ -6,6 +6,7 @@ package src.graph;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Time complexity:O();
@@ -13,7 +14,9 @@ import java.util.List;
  * Description: TODO
  */
 public class DFS {
-
+    /**
+     * dfs with created class
+     */
     public static void dfs(List<GraphNode> list) {
         HashSet<GraphNode> visited = new HashSet<>();
         for (GraphNode node : list) {
@@ -27,6 +30,9 @@ public class DFS {
         helper(node, new HashSet<>());
     }
 
+    /**
+     * recursion
+     */
     private static void helper(GraphNode node, HashSet<GraphNode> visited) {
         System.out.println(node.label);
         visited.add(node);
@@ -36,9 +42,28 @@ public class DFS {
             }
         }
     }
+    /**
+     * iteration
+     */
+    public static void dfsIteration(GraphNode graphNode) {
+        Stack<GraphNode> stack = new Stack<>();
+        HashSet<GraphNode> visited = new HashSet<>();
+        stack.push(graphNode);
+        visited.add(graphNode);
+        while (!stack.isEmpty()) {
+            GraphNode node = stack.pop();
+            System.out.println(node.label);
+            for (GraphNode neighbor : node.neighbors) {
+                if (!visited.contains(neighbor)) {
+                    stack.push(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+    }
 
     /**
-     * dfs with matrix
+     * dfs with matrix recursion
      */
     public static void dfsMatrix(int[][] matrix) {
         int[] visited = new int[matrix.length];
@@ -51,10 +76,35 @@ public class DFS {
 
     private static void helper(int[][] matrix, int[] visited, int vertex) {
         visited[vertex] = 1;
+        System.out.println(vertex);
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[vertex][i] == 1) {
                 if (visited[i] == 0) {
                     helper(matrix, visited, i);
+                }
+            }
+        }
+    }
+    /**
+     * dfs with matrix iteration
+     */
+    public static void dfsMatrixIteration(int[][] matrix) {
+        int[] visited = new int[matrix.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                stack.push(i);
+                while (!stack.isEmpty()) {
+                    Integer vertex = stack.pop();
+                    for (int j = 0; j < matrix.length; j++) {
+                        if (matrix[vertex][j] == 1) {
+                            if (visited[j] == 0) {
+                                stack.push(j);
+                                visited[j] = 1;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -69,5 +119,6 @@ public class DFS {
         a.neighbors.add(c);
 
         dfs2(a);
+        dfsIteration(a);
     }
 }
