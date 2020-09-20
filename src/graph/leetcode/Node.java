1a2e9eadb3edb264b8679ea1c9aff30591cbe393
kqiu10 <qiukan97@gmail.com>
2020-09-19 20:38:13 -0400

    Update Node.java

--- src/graph/leetcode/Node.java
+++ src/graph/leetcode/Node.java
@@ -4,10 +4,30 @@ package src.graph.leetcode;
  * Question Description
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Time complexity:O();
  * Space complexity: O();
  * Description: TODO
  */
 public class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 }
