53c060c90b8b22a8e9c634167a446b475c73eb7c
kqiu10 <qiukan97@gmail.com>
2020-09-22 17:43:20 -0400

    Update _127_WordLadder.java

--- leetcode/Graph图/BFS/_127_WordLadder.java
+++ leetcode/Graph图/BFS/_127_WordLadder.java
@@ -37,10 +37,97 @@ package leetcode.Graph图.BFS;
  * Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
  */
 
+import java.util.*;
+
 /**
- * Time complexity:O();
- * Space complexity: O();
- * Description: TODO
+ * Time complexity:O(n * max(len));
+ * Space complexity: O(n);
+ * Description: BFS
+ * Ex
+ *      beginWord = "hit"
+ *      endWord = "cog"
+ *      wordList = ["hot","dot","dog","lot","log"]
+ *      hit -> hot
+ *      level = 1 cur:1->0, next:0->1
+ *                cur:0->1. next:1->0
+ *         hot->dot / lot
+ *         level = 2 cur:1->0  next:0->2
+ *                   cur:0->2  next:2->0
+ *     cur 同一级别个数   next 下一级别个数
  */
 public class _127_WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> set = new HashSet<>(wordList);
+        if (set.contains(beginWord)) {
+            set.remove(beginWord);
+        }
+        Queue<String> queue = new LinkedList<>();
+        int level = 1;
+        int curNum = 1;
+        int nextNum = 0;
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            String word = queue.poll();
+            curNum--;
+            for (int i = 0; i < word.length(); i++) {
+                char[] wordUnit = word.toCharArray();
+                for (char j = 'a'; j <= 'z'; j++) {
+                    wordUnit[i] = j;
+                    String temp = new String(wordUnit);
+                    if (set.contains(temp)) {
+                        if (temp.equals(endWord)) {
+                            return level + 1;
+                        }
+                        nextNum++;
+                        queue.offer(temp);
+                        set.remove(temp);
+                    }
+                }
+            }
+            if (curNum == 0) {
+                curNum = nextNum;
+                nextNum = 0;
+                level++;
+            }
+        }
+        return 0;
+    }
+}
+/**
+ * Time complexity:O(n * max(len));
+ * Space complexity: O(n);
+ * Description: BFS HashMap (recommended)
+ */
+class WordLadderII {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> set = new HashSet<>(wordList);
+        if (set.contains(beginWord)) {
+            set.remove(beginWord);
+        }
+        Queue<String> queue = new LinkedList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(beginWord, 1);
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            String word = queue.poll();
+            int curLevel = map.get(word);
+            for (int i = 0; i < word.length(); i++) {
+                char[] curArray = word.toCharArray();
+                for (char j = 'a'; j <= 'z'; j++) {
+                    curArray[i] = j;
+                    String temp = new String(curArray);
+                    if (set.contains(temp)) {
+                        if (temp.equals(endWord)) {
+                            return curLevel + 1;
+                        }
+                        map.put(temp, curLevel + 1);
+                        queue.offer(temp);
+                        set.remove(temp);
+                    }
+                }
+            }
+        }
+        return 0;
+
+    }
 }
