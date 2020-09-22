483f7469ff4f3e0c3def7d3074db7235a68fc70a
kqiu10 <qiukan97@gmail.com>
2020-09-21 22:33:28 -0400

    Create _323_NumberofConnectedComponentsinanUndirectedGraphFinal.java

---
+++ src/unionFind/leetcode/_323_NumberofConnectedComponentsinanUndirectedGraphFinal.java
@@ -0,0 +1,82 @@
+package src.unionFind.leetcode;
+/**
+ * Date: 9/21/20
+ * Question Description
+ * Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of
+ * nodes), write a function to find the number of connected components in an undirected graph.
+ *
+ * Example 1:
+ *
+ * Input: n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
+ *
+ *      0          3
+ *      |          |
+ *      1 --- 2    4
+ *
+ * Output: 2
+ * Example 2:
+ *
+ * Input: n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+ *
+ *      0           4
+ *      |           |
+ *      1 --- 2 --- 3
+ *
+ * Output:  1
+ *
+ */
+
+/**
+ * Time complexity:O();
+ * Space complexity: O();
+ * Description: TODO
+ */
+public class _323_NumberofConnectedComponentsinanUndirectedGraphFinal {
+
+    public static   int countComponents(int n, int[][] edges) {
+        int res = n;
+        int[] parents = new int[n];
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+        for (int[] pair : edges) {
+            int xRoot = find(pair[0], parents);
+            int yRoot = find(pair[1], parents);
+            if (xRoot != yRoot) {
+                parents[xRoot] = yRoot;
+                res--;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 面试推荐
+     * @param index
+     * @param parents
+     * @return
+     */
+    public static int find(int index, int[] parents) {
+        while (index != parents[index]) {
+            index = parents[index];
+        }
+        return index;
+    }
+
+    /**
+     * 效率更高，路径压缩
+     */
+    public int find2(int index, int[] parents) {
+        while (index != parents[index]) {
+           parents[index] = parents[parents[index]];
+           index = parents[index];
+        }
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        countComponents(5, new int[][]{{0, 1}, {1, 2}, {3, 4}});
+    }
+
+}
