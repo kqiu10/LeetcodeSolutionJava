5d7cd01809b26a81f52af15d6d56b8444fe1ab0a
kqiu10 <qiukan97@gmail.com>
2020-09-30 19:46:10 -0400

    finish bitwise

--- leetcode二刷/test.java
+++ leetcode二刷/test.java
@@ -3,6 +3,9 @@
  * Question Description
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Description: TODO
  * Time complexity:O();
@@ -10,19 +13,11 @@
 
  */
 public class test {
-    // you need treat n as an unsigned value
-    public static int reverseBits(int n) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res |= (n & 1);
-            n >>= 1;
-        }
-        return res;
 
     }
 
     public static void main(String[] args) {
         reverseBits(28);
     }
+
 }
