6a901fb9637273b20ae23db33fa4021770ce39a4
kqiu10 <qiukan97@gmail.com>
2020-09-30 22:18:28 -0400

    Update test.java

--- leetcode二刷/test.java
+++ leetcode二刷/test.java
@@ -13,35 +13,19 @@ import java.util.List;
 
  */
 public class test {
-    public int myAtoi(String str) {
-        if (str == null || str.length() == 0) return 0;
-        str = str.trim();
-        int start = 0;
-        int sign = 1;
-        long res = 0;
-        char first = str.charAt(0);
-        if (first == '+') {
-            start = 1;
-            sign = 1;
-        } else if (first == '-') {
-            start = 1;
-            sign = -1;
+    public int[] plusOne(int[] digits) {
+        int sign = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
         }
-        for (int i = start; i < str.length(); i++) {
-            if (!Character.isDigit(str.charAt(i))) {
-                return sign * (int)res;
-            }
-             res = res * 10 + str.charAt(i) - '0';
-            if (sign == 1 && res > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            }
-            if (sign == -1 && res > Integer.MAX_VALUE) {
-                return Integer.MIN_VALUE;
-            }
-
-        }
-        return sign * (int)res;
-
+        int[] final = new int[digits.length + 1];
+        final[0] = 1;
+        return final;
     }
 
 }
