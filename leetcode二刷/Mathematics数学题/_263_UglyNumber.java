ecc221771ebae7660be0e40ecf8a1eb9d3f72400
kqiu10 <qiukan97@gmail.com>
2020-10-01 15:17:29 -0400

    Update _263_UglyNumber.java

--- leetcode二刷/Mathematics数学题/_263_UglyNumber.java
+++ leetcode二刷/Mathematics数学题/_263_UglyNumber.java
@@ -11,7 +11,7 @@ package leetcode.Mathmatics数学题;
  * Space complexity: O(1);
  * Description: TODO
  */
-public class _263_UglyNumber263 {
+public class _263_UglyNumber {
     public boolean isUgly(int num) {
         for (int i = 2; i < 6 && num > 0; i++) {
             while (num % i == 0) {
