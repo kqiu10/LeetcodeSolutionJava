09238a3182689bf0f6cbbba6d99b7fc772169cec
kqiu10 <qiukan97@gmail.com>
2020-10-02 11:07:51 -0400

    update 300

--- leetcode二刷/ArrayList数组/_300_LongestIncreasingSubsequence.java
+++ leetcode二刷/ArrayList数组/_300_LongestIncreasingSubsequence.java
@@ -30,6 +30,19 @@ package leetcode.数组;
  * size相当于res数组的长度
  */
 public class _300_LongestIncreasingSubsequence {
+    /**
+     * Time complexity:O(nlogn);
+     * Space complexity: O(n);
+     *
+     *
+     * Description:
+     * 遍历整个数组，对任意新的元素X，如果X比答案数组的最后一个值大，就加入答案数组中，否则就在答案数组中寻找一个比X大的元素用二分法进行替换，
+     * 所以进行替换代表我们不改变答案数组的长度，但是我们改变了潜在的最佳答案，想法是尝试使数组中每个位置的值尽可能的小，数组的实际顺序仅在我们添加数字时才会更改，
+     * 否则只是一个“虚拟更改”，表示我们不更改当前顺序，但尝试减小每个数字，以便我们有更大的机会附加更多数字
+     *
+     * i,j 相当于res数组的起点和终点；
+     * size相当于res数组的长度
+     */
     public static int lengthOfLIS(int[] nums) {
         int[] res = new int[nums.length];
         int size = 0;
@@ -50,6 +63,31 @@ public class _300_LongestIncreasingSubsequence {
         }
         return size;
 
+    }
+
+    /**
+     * Descirption: DP, more easy and more reliable way
+     * Time complexity:O(n^2);
+     * Space complexity: O(n);
+     */
+
+    public int lengthOfLISII(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int res = 1;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+
+        for (int i = 1; i < dp.length; i++) {
+            int maxVal = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxVal = Math.max(maxVal, dp[j]);
+                }
+            }
+            dp[i] = maxVal + 1;
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
