d886f6983675608f79c1dac51a91d9c78f68fff3
kqiu10 <qiukan97@gmail.com>
2020-10-06 13:12:38 -0400

    Create _113_PathSumII.java

---
+++ leetcode一刷/Tree树/Preorder先序遍历/_113_PathSumII.java
@@ -0,0 +1,61 @@
+package Tree树.Preorder先序遍历;
+/**
+ * Date: 10/6/20
+ * Question Description
+ */
+
+import Tree树.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+ Note: A leaf is a node with no children.
+
+ Example:
+
+ Given the below binary tree and sum = 22,
+
+       5
+      / \
+     4   8
+    /   / \
+   11  13  4
+  /  \    / \
+ 7    2  5   1
+ Return:
+
+ [
+ [5,4,11,2],
+ [5,8,4,5]
+ ]
+
+ * Time complexity:O(n);
+ * Space complexity: O(h);
+
+ */
+public class _113_PathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, new ArrayList<>(), root, sum);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> list, TreeNode root, int sum) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                res.add(new ArrayList<>(list));
+            }
+        }
+
+        helper(res, list, root.left, sum - root.val);
+        helper(res, list, root.right, sum - root.val);
+        list.remove(list.size() - 1);
+    }
+}
