a8b4ad9bb5a9b4c1a1fadc8607b862801859825d
kqiu10 <qiukan97@gmail.com>
2020-10-06 12:30:38 -0400

    Create _102_BinaryTreeLevelOrderTraversal.java

---
+++ leetcode一刷/Tree树/Basic基础/_102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,45 @@
+package Tree树.Basic基础;
+/**
+ * Date: 10/6/20
+ * Question Description
+ */
+
+import Tree树.TreeNode;
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+public class _102_BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                list.add(cur.val);
+            }
+            res.add(list);
+        }
+        return res;
+
+    }
+}
