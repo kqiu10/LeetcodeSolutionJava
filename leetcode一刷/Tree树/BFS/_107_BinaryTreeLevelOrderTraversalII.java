4521f2b828f00dddeafbfd85ff321b9deb65d600
kqiu10 <qiukan97@gmail.com>
2020-10-07 18:08:28 -0400

    Create _107_BinaryTreeLevelOrderTraversalII.java

---
+++ leetcode一刷/Tree树/BFS/_107_BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,57 @@
+package Tree树.BFS;
+/**
+ * Date: 10/7/20
+ * Question Description
+ */
+
+import Tree树.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Description:
+ Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+ For example:
+ Given binary tree [3,9,20,null,null,15,7],
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ return its bottom-up level order traversal as:
+ [
+ [15,7],
+ [9,20],
+ [3]
+ ]
+ * Time complexity:O(1);
+ * Space complexity: O(h);
+
+ */
+public class _107_BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                list.add(cur.val);
+            }
+            res.addFirst(list);
+        }
+        return res;
+    }
+}
