9bb8047f885b866d60fcb93e5a9fc1f7396055bb
kqiu10 <qiukan97@gmail.com>
2020-10-07 19:05:39 -0400

    Create _199_BinaryTreeRightSideView.java

---
+++ leetcode一刷/Tree树/BFS/_199_BinaryTreeRightSideView.java
@@ -0,0 +1,28 @@
+package Tree树.BFS;
+/**
+ * Date: 10/7/20
+ * Question Description
+ */
+
+/**
+ Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ Example:
+
+ Input: [1,2,3,null,5,null,4]
+ Output: [1, 3, 4]
+ Explanation:
+
+            1            <---
+        /   \
+        2     3         <---
+        \     \
+        5     4       <---
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+public class _199_BinaryTreeRightSideView {
+
+}
