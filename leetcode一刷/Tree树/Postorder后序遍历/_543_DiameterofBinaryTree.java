22c72c8b2bcaabf1249744fcf88bd0d733f9a970
kqiu10 <qiukan97@gmail.com>
2020-10-09 00:03:10 -0400

    Create _543_DiameterofBinaryTree.java

---
+++ leetcode一刷/Tree树/Postorder后序遍历/_543_DiameterofBinaryTree.java
@@ -0,0 +1,41 @@
+package Tree树.Postorder后序遍历;
+/**
+ * Date: 10/8/20
+ * Question Description
+ Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+ Example:
+ Given a binary tree
+ 1
+ / \
+ 2   3
+ / \
+ 4   5
+ Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+ Note: The length of path between two nodes is represented by the number of edges between them.
+ */
+
+import Tree树.TreeNode;
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+
+ */
+public class _543_DiameterofBinaryTree {
+    private int res = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return res;
+        helper(root);
+        return res;
+    }
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int l = helper(root.left);
+        int r = helper(root.right);
+        res = Math.max(res, l + r);
+        return Math.max(l, r) + 1;
+    }
+}
