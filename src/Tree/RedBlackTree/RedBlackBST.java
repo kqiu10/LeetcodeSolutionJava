c9c05373d88e25ff483cb00d19a049f235e9d814
kqiu10 <qiukan97@gmail.com>
2020-10-17 19:54:50 -0400

    Create RedBlackBST.java

---
+++ src/Tree/RedBlackTree/RedBlackBST.java
@@ -0,0 +1,51 @@
+package Tree.RedBlackTree;
+/**
+ * Date: 10/17/20
+ * Question Description
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+public class RedBlackBST<Key extends  Comparable<Key>, value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+    private class Node {
+        private Key key;
+        private value value;
+        private Node left, right;
+        private boolean color;
+        private int size;
+
+        public Node(Key key, value value, boolean color, int size) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+            this.size = size;
+        }
+    }
+
+    public RedBlackBST() {
+
+    }
+    private boolean isRed(Node node) {
+        if (node == null) return  false;
+        return node.color == RED;
+    }
+    private int size(Node node) {
+        if (node == null) return 0;
+        return node.size;
+    }
+    public int size() {
+        return size(root);
+    }
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+}
