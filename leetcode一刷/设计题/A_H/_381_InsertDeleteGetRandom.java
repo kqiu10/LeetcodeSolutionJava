a06c9a0facf5502032378c230929a993cd2028b8
kqiu10 <qiukan97@gmail.com>
2020-10-19 16:28:27 -0400

    Create _381_InsertDeleteGetRandom.java

---
+++ leetcode一刷/设计题/A_H/_381_InsertDeleteGetRandom.java
@@ -0,0 +1,101 @@
+package 设计题.A_H;
+/**
+ * Date: 10/19/20
+ * Question Description
+ Design a data structure that supports all following operations in average O(1) time.
+
+ Note: Duplicate elements are allowed.
+ insert(val): Inserts an item val to the collection.
+ remove(val): Removes an item val from the collection if present.
+ getRandom: Returns a random element from current collection of elements. The probability of each element being returned is linearly related to the number of same value the collection contains.
+ Example:
+
+ // Init an empty collection.
+ RandomizedCollection collection = new RandomizedCollection();
+
+ // Inserts 1 to the collection. Returns true as the collection did not contain 1.
+ collection.insert(1);
+
+ // Inserts another 1 to the collection. Returns false as the collection contained 1. Collection now contains [1,1].
+ collection.insert(1);
+
+ // Inserts 2 to the collection, returns true. Collection now contains [1,1,2].
+ collection.insert(2);
+
+ // getRandom should return 1 with the probability 2/3, and returns 2 with the probability 1/3.
+ collection.getRandom();
+
+ // Removes 1 from the collection, returns true. Collection now contains [1,2].
+ collection.remove(1);
+
+ // getRandom should return 1 and 2 both equally likely.
+ collection.getRandom();
+ */
+
+import java.util.*;
+
+/**
+ * Description:
+    Ex:
+    list : 2 3 3 4
+    map : 2 : 0
+          3 : 1 2
+          4 : 3
+    remove val = 3
+    result
+    list : 2 3 4
+    map : 2 : 0
+          3 : 1
+          4 : 2
+ * Time complexity:O(1);
+ * Space complexity: O(n);
+
+ */
+public class _381_InsertDeleteGetRandom {
+    HashMap<Integer, HashSet<Integer>> map;
+    Random rnd;
+    List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public _381_InsertDeleteGetRandom() {
+        map = new HashMap<>();
+        rnd = new Random();
+        list = new ArrayList<>();
+
+    }
+
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contain = map.containsKey(val);
+        if (!contain) {
+            map.put(val, new HashSet<>());
+
+        }
+        map.get(val).add(list.size());
+        list.add(val);
+        return !contain;
+
+
+    }
+
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        int index = map.get(val).iterator().next();
+        map.get(val).remove(index);
+        if (map.get(val).size() == 0) map.remove(val);
+        int lastVal = list.remove(list.size() - 1);
+        if (index != list.size()) {
+            list.set(index, lastVal);
+            map.get(lastVal).remove(list.size());
+            map.get(lastVal).add(index);
+        }
+        return true;
+    }
+
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return list.get(rnd.nextInt(list.size()));
+
+    }
+}
