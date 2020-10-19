bb0b2b8259383b130471505fecd5b549dc6be741
kqiu10 <qiukan97@gmail.com>
2020-10-19 11:59:36 -0400

    Update _284_PeekingIterator.java

--- leetcode一刷/设计题/Iterator/_284_PeekingIterator.java
+++ leetcode一刷/设计题/Iterator/_284_PeekingIterator.java
@@ -15,11 +15,43 @@ package 设计题.Iterator;
 
  */
 
+import java.util.Iterator;
+
 /**
  * Description: TODO
  * Time complexity:O();
  * Space complexity: O();
 
  */
-public class _284_PeekingIterator {
+class _284_PeekingIterator implements Iterator<Integer> {
+    private Iterator<Integer> iter;
+    private Integer next = null;
+    public _284_PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        iter = iterator;
+        if (iter.hasNext()) {
+            next = iter.next();
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer res = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return res;
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
 }
