37f3994327ff264fef3e2b095455f61c2f156105
kqiu10 <qiukan97@gmail.com>
2020-10-24 18:35:45 -0400

    Create _279_PerfectSquares.java

---
+++ leetcode一刷/DP动态规划/实现题/_279_PerfectSquares.java
@@ -0,0 +1,41 @@
+package DP动态规划.实现题;
+/**
+ * Date: 10/24/20
+ * Question Description
+ Given a positive integer n,
+ find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+ Example 1:
+ Input: n = 12
+ Output: 3
+ Explanation: 12 = 4 + 4 + 4.
+
+ Example 2:
+ Input: n = 13
+ Output: 2
+ Explanation: 13 = 4 + 9.
+ */
+
+import java.util.Arrays;
+
+/**
+ * Description: TODO
+ * Time complexity:O(n * sqrt(n));
+ * Space complexity: O(n);
+
+ */
+public class _279_PerfectSquares {
+    public int numSquares(int n) {
+        int[] res = new int[n + 1];
+        Arrays.fill(res, Integer.MAX_VALUE);
+        res[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= n; j++) {
+                res[i] = Math.min(res[i], res[i - j * j] + 1);
+            }
+        }
+        return res[n];
+
+    }
+}
+
