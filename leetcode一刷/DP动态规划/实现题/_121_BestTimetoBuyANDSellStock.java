e64bba5d971f5822333b3d16e2430d4d7ae34c99
kqiu10 <qiukan97@gmail.com>
2020-10-24 18:39:16 -0400

    Create _121_BestTimetoBuyANDSellStock.java

---
+++ leetcode一刷/DP动态规划/实现题/_121_BestTimetoBuyANDSellStock.java
@@ -0,0 +1,41 @@
+package DP动态规划.实现题;
+/**
+ * Date: 10/24/20
+ * Question Description
+ Say you have an array for which the ith element is the price of a given stock on day i.
+
+ If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+ Note that you cannot sell a stock before you buy one.
+
+ Example 1:
+
+ Input: [7,1,5,3,6,4]
+ Output: 5
+ Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ Not 7-1 = 6, as selling price needs to be larger than buying price.
+ Example 2:
+
+ Input: [7,6,4,3,1]
+ Output: 0
+ Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+
+ */
+public class _121_BestTimetoBuyANDSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+        int min = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+        return profit;
+    }
+}
