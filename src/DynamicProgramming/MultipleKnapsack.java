16589f25a5fb41de0479fa403c4ae4230d27c512
kqiu10 <qiukan97@gmail.com>
2020-10-25 22:28:49 -0400

    Update MultipleKnapsack.java

--- src/DynamicProgramming/MultipleKnapsack.java
+++ src/DynamicProgramming/MultipleKnapsack.java
@@ -11,4 +11,18 @@ package DynamicProgramming;
 
  */
 public class MultipleKnapsack {
+    public int knapsackMultiple(int[] weight, int[] value, int[] nums, int C, int N) {
+        int[] memo = new int[C + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = C; j >= weight[i]; j--) {
+                for (int k = 0; k <= nums[i]; k++) {
+                    if (j - k * weight[i] >= 0) {
+                        memo[j] = Math.max(memo[j], k * value[i] + memo[j - k * weight[i]]);
+                    }
+                }
+            }
+        }
+        return memo[C];
+    }
 }
