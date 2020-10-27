2bfe16710b91b6a6ae84213259e4a4cf17bf26bf
kqiu10 <qiukan97@gmail.com>
2020-10-27 16:46:34 -0400

    Create _256_PaintHouse.java

---
+++ leetcode一刷/DP动态规划/实现题/_256_PaintHouse.java
@@ -0,0 +1,41 @@
+package DP动态规划.实现题;
+/**
+ * Date: 10/27/20
+ * Question Description
+ There is a row of n houses, where each house can be painted one of three colors: red, blue, or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+ The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with the color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+ Example 1:
+
+ Input: costs = [[17,2,17],[16,16,5],[14,3,19]]
+ Output: 10
+ Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
+ Minimum cost: 2 + 5 + 3 = 10.
+ Example 2:
+
+ Input: costs = []
+ Output: 0
+ Example 3:
+
+ Input: costs = [[7,6,2]]
+ Output: 2
+ /
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+
+ */
+public class _256_PaintHouse {
+    public int minCost(int[][] costs) {
+        if (costs == null || costs.length == 0) return 0;
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        int n = costs.length - 1;
+        return Math.min(Math.min(costs[n][0], costs[n][1]), costs[n][2]);
+    }
+}
