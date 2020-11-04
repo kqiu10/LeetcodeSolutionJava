a0052ba347ba6e09c26d078f98c3faa5d539c93a
kqiu10 <qiukan97@gmail.com>
2020-11-04 17:15:27 -0500

    Create _40_CombinationSumII.java

---
+++ leetcode精刷250/_40_CombinationSumII.java
@@ -0,0 +1,61 @@
+/**
+ * Date: 11/4/20
+ * Question Description
+ Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+ Each number in candidates may only be used once in the combination.
+
+ Note: The solution set must not contain duplicate combinations.
+
+ Example 1:
+
+ Input: candidates = [10,1,2,7,6,1,5], target = 8
+ Output:
+ [
+ [1,1,6],
+ [1,2,5],
+ [1,7],
+ [2,6]
+ ]
+ Example 2:
+
+ Input: candidates = [2,5,2,1,2], target = 5
+ Output:
+ [
+ [1,2,2],
+ [5]
+ ]
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Description: TODO
+ * Time complexity:O(2^n);
+ * Space complexity: O(n);
+
+ */
+public class _40_CombinationSumII {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        helper(res, candidates, new ArrayList<>(), target, 0);
+        return res;
+    }
+    private void helper(List<List<Integer>> res, int[] candidates, List<Integer> list, int target, int index) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            if (i != index && candidates[i] == candidates[i - 1]) continue;
+            list.add(candidates[i]);
+            helper(res, candidates, list, target - candidates[i], i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
