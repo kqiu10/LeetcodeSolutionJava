7ac703003d033617e9bbb6179d5401b44cf91784
kqiu10 <qiukan97@gmail.com>
2020-11-04 21:41:20 -0500

    Update _38_CountandSay.java

--- leetcode精刷250/_38_CountandSay.java
+++ leetcode精刷250/_38_CountandSay.java
@@ -7,21 +7,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Description: TODO
- * Time complexity:O();
- * Space complexity: O();
-
+ * Time complexity:O(n^2);
+ * Space complexity: O(n);
+ * Description:
+ *     1 count = 0 c = "1" res = "1"
+ *     2 count = 1 c = "1" res = "11"
+ *     3 j = 0 count = 1;
+ *       j = 1 count = 2;
+ *       res = "21"
+ *     4 j = 0 c = "2" count = 1;
+ *       j = 1 c = "1" res = "12" count = 1;
+ *       j = 2 res = "1211"
+ *     5 j = 0 c = "1" count = 1;
+ *       j = 1 c = "2" res = "11" count = 1;
+ *       j = 2 c = "1"  res = "1112" count = 1;
+ *       j = 3 c = "1" count = 2;
+ *       j = 4 c = "1" res = "111221"
  */
 public class _38_CountandSay {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (candidates == null || candidates.length == 0) return res;
-        helper(res, candidates, new ArrayList<>(), target);
-        return res;
-    }
-    private void helper(List<List<Integer>> res, int[] candidates, List<Integer> list, int target) {
-        if (target == 0) {
-            res.add(new ArrayList<>(list));
+    public String countAndSay(int n) {
+        int i = 1;
+        String res = "1";
+        while (i < n) {
+            int count  = 0;
+            StringBuilder sb = new StringBuilder();
+            char c = res.charAt(0);
+            for (int j = 0; j <= res.length(); j++) {
+                if (j != res.length() && c == res.charAt(j)) {
+                    count++;
+                } else {
+                    sb.append(count);
+                    sb.append(c);
+                    if (j != res.length()) {
+                        count = 1;
+                        c = res.charAt(j);
+                    }
+                }
+            }
+            res = sb.toString();
+            i++;
         }
+        return res;
+
     }
 }
