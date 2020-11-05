1118791249dbc60a3c80b7d88454d39f66c97ae9
kqiu10 <qiukan97@gmail.com>
2020-11-05 13:10:15 -0500

    Create _42_TrappingRainWater.java

---
+++ leetcode精刷250/_42_TrappingRainWater.java
@@ -0,0 +1,32 @@
+/**
+ * Date: 11/5/20
+ * Question Description
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+
+ */
+public class _42_TrappingRainWater {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
