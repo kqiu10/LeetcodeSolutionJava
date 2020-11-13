dc30efbf355969b6c3df2ed8c104645bca702ec9
kqiu10 <qiukan97@gmail.com>
2020-11-12 22:30:47 -0500

    Create _116_PopulatingNextRightPointersinEachNode.java

---
+++ leetcode精刷250/_116_PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,50 @@
+/**
+ * Date: 11/12/20
+ * Question Description
+ You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+ struct Node {
+ int val;
+ Node *left;
+ Node *right;
+ Node *next;
+ }
+ Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+ Initially, all next pointers are set to NULL.
+
+ Follow up:
+
+ You may only use constant extra space.
+ Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+
+
+ Example 1:
+
+ Input: root = [1,2,3,4,5,6,7]
+ Output: [1,#,2,3,#,4,5,6,7,#]
+ Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ */
+
+import Tree树.难且重要.Node;
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(h);
+
+ */
+public class _116_PopulatingNextRightPointersinEachNode {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+        if (root.next != null && root.right != null) {
+            root.right.next = root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+}
