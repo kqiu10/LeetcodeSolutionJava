93df7babc35ce9d0bee4571456abf0955f3c9be2
kqiu10 <qiukan97@gmail.com>
2020-11-16 17:42:59 -0500

    Create _150_EvaluateReversePolishNotation.java

---
+++ leetcode精刷250/_150_EvaluateReversePolishNotation.java
@@ -0,0 +1,34 @@
+/**
+ * Date: 11/16/20
+ * Question Description
+ */
+
+import java.util.Stack;
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+
+ */
+public class _150_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                int last = stack.pop();
+                stack.push(stack.pop() - last);
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int last = stack.pop();
+                stack.push(stack.pop() / last);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
