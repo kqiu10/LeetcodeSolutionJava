72b299500215d3aa6a17f0e695c281ac3df31a81
kqiu10 <qiukan97@gmail.com>
2020-11-18 15:49:32 -0500

    Create _157_ReadNCharactersGivenRead4.java

---
+++ leetcode精刷250/_157_ReadNCharactersGivenRead4.java
@@ -0,0 +1,21 @@
+/**
+ * Date: 11/18/20
+ * Question Description
+ Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
+ Method read4:
+
+ The API read4 reads 4 consecutive characters from the file, then writes those characters into the buffer array buf4.
+
+ The return value is the number of actual characters read.
+
+ Note that read4() has its own file pointer, much like FILE *fp in C.
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+public class _157_ReadNCharactersGivenRead4 {
+}
