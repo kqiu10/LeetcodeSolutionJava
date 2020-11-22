186bc08446d7e522b3c23c470748480d4615c2c7
kqiu10 <qiukan97@gmail.com>
2020-11-21 21:14:54 -0500

    Create _200_NumberOfIslands.java

---
+++ leetcode精刷250/_200_NumberOfIslands.java
@@ -0,0 +1,38 @@
+/**
+ * Date: 11/21/20
+ * Question Description
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(m * n);
+ * Space complexity: O(m * n);
+
+ */
+public class _200_NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
