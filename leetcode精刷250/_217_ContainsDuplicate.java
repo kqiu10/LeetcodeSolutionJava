2acbec52e67e9ef060534fbf84a6c09a41cabb2d
kqiu10 <qiukan97@gmail.com>
2020-11-21 23:45:33 -0500

    Create _217_ContainsDuplicate.java

---
+++ leetcode精刷250/_217_ContainsDuplicate.java
@@ -0,0 +1,38 @@
+/**
+ * Date: 11/21/20
+ * Question Description
+ Given an array of integers, find if the array contains any duplicates.
+
+ Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+ Example 1:
+
+ Input: [1,2,3,1]
+ Output: true
+ Example 2:
+
+ Input: [1,2,3,4]
+ Output: false
+ Example 3:
+
+ Input: [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+ */
+
+import java.util.Arrays;
+
+/**
+ * Description: TODO
+ * Time complexity:O(nlogn);
+ * Space complexity: O(1);
+
+ */
+public class _217_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) return true;
+        }
+        return false;
+    }
+}
