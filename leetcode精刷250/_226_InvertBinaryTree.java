c44c0ad3ba7d4eeb3908d5c8c28b9d80e6141977
kqiu10 <qiukan97@gmail.com>
2020-11-23 20:56:40 -0500

    create _232_

--- leetcode精刷250/_226_InvertBinaryTree.java
+++ leetcode精刷250/_226_InvertBinaryTree.java
@@ -1,6 +1,24 @@
 /**
  * Date: 11/23/20
  * Question Description
+ Invert a binary tree.
+
+ Example:
+
+ Input:
+
+      4
+    /   \
+   2     7
+  / \   / \
+ 1   3 6   9
+ Output:
+
+      4
+    /   \
+   7     2
+  / \   / \
+ 9   6 3   1
  */
 
 import Tree树.TreeNode;
--- leetcode精刷250/_231_PowerofTwo.java
+++ leetcode精刷250/_231_PowerofTwo.java
@@ -1,6 +1,9 @@
 /**
  * Date: 11/23/20
  * Question Description
+ Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+ An integer n is a power of two, if there exists an integer x such that n == 2x.
  */
 
 /**
