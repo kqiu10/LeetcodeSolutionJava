571e08028e2153a733a86341070913d0169b1071
kqiu10 <qiukan97@gmail.com>
2020-11-26 16:14:48 -0500

    Create _238_PruductofArrayExceptSelf.java

---
+++ leetcode精刷250/_238_PruductofArrayExceptSelf.java
@@ -0,0 +1,36 @@
+/**
+ * Date: 11/26/20
+ * Question Description
+ Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+ Example:
+
+ Input:  [1,2,3,4]
+ Output: [24,12,8,6]
+ Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+ Note: Please solve it without division and in O(n).
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+
+ */
+public class _238_PruductofArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) return nums;
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
