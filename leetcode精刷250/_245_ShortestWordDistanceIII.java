b180b44bfe835e143edaa1e1e2bfb4c1dafa7623
kqiu10 <qiukan97@gmail.com>
2020-11-26 17:02:27 -0500

    Create _245_ShortestWordDistanceIII.java

---
+++ leetcode精刷250/_245_ShortestWordDistanceIII.java
@@ -0,0 +1,43 @@
+/**
+ * Date: 11/26/20
+ * Question Description
+ Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+ word1 and word2 may be the same and they represent two individual words in the list.
+
+ Example:
+ Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+ Input: word1 = “makes”, word2 = “coding”
+ Output: 1
+ Input: word1 = "makes", word2 = "makes"
+ Output: 3
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+
+ */
+public class _245_ShortestWordDistanceIII {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int a = -1, b = -1;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                a = i;
+            }
+            if (words[i].equals(word2)) {
+                if (word1.equals(word2)) {
+                    a = b;
+                }
+                b = i;
+            }
+            if (a != -1 && b != -1) {
+                res = Math.min(res, Math.abs(a - b));
+            }
+        }
+        return res;
+    }
+}
