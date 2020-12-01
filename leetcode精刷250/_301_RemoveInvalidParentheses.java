d307f7af58be70447d2d1c6da0d90e10d9b99a68
kqiu10 <qiukan97@gmail.com>
2020-12-01 12:19:54 -0500

    Create _301_RemoveInvalidParentheses.java

---
+++ leetcode精刷250/_301_RemoveInvalidParentheses.java
@@ -0,0 +1,29 @@
+/**
+ * Date: 12/1/20
+ * Question Description
+ Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+ Note: The input string may contain letters other than the parentheses ( and ).
+
+ Example 1:
+
+ Input: "()())()"
+ Output: ["()()()", "(())()"]
+ Example 2:
+
+ Input: "(a)())()"
+ Output: ["(a)()()", "(a())()"]
+ Example 3:
+
+ Input: ")("
+ Output: [""]
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+public class _301_RemoveInvalidParentheses {
+}
