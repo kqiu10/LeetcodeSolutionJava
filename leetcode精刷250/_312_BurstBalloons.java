22c7d127588fd09f55b3d1f1fbfeeeec0b347806
kqiu10 <qiukan97@gmail.com>
2020-12-04 13:46:50 -0500

    Update _312_BurstBalloons.java

--- leetcode精刷250/_312_BurstBalloons.java
+++ leetcode精刷250/_312_BurstBalloons.java
@@ -24,4 +24,23 @@
 
  */
 public class _312_BurstBalloons {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
+        }
+        arr[0] = arr[n + 1] = 1;
+        int[][] dp = new int[n + 2][n + 2];
+        return helper(1, n, arr, dp);
+    }
+    private int helper(int i, int j, int[] nums, int[][] dp) {
+        if (i > j) return 0;
+        if (dp[i][j] > 0) return dp[i][j];
+        for (int x = i; x <= j; x++) {
+            dp[i][j] = Math.max(dp[i][j], helper(i, x - 1, nums, dp) + nums[i - 1] * nums[x] * nums[j + 1]
+                    + helper(x + 1, j, nums, dp));
+        }
+        return dp[i][j];
+    }
 }
