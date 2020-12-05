9230bf10ee223e4f053f778e6c2f0f70aecaea1e
kqiu10 <qiukan97@gmail.com>
2020-12-05 17:31:52 -0500

    Create _350_IntersectionOfTwoArraysII.java

---
+++ leetcode精刷250/_350_IntersectionOfTwoArraysII.java
@@ -0,0 +1,46 @@
+/**
+ * Date: 12/5/20
+ * Question Description
+ Given two arrays, write a function to compute their intersection.
+
+ Example 1:
+
+ Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ Output: [2,2]
+ Example 2:
+
+ Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ Output: [4,9]
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(n);
+
+ */
+public class _350_IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums2) {
+            int cur = map.getOrDefault(num, 0);
+            if (cur > 0) {
+                list.add(num);
+                map.put(num, cur - 1);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
