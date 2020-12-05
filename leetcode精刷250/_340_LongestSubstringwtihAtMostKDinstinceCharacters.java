e3a3d934b2f92fb28997d1bc273947ea6b98fdc4
kqiu10 <qiukan97@gmail.com>
2020-12-05 11:42:05 -0500

    Create _340_LongestSubstringwtihAtMostKDinstinceCharacters.java

---
+++ leetcode精刷250/_340_LongestSubstringwtihAtMostKDinstinceCharacters.java
@@ -0,0 +1,40 @@
+/**
+ * Date: 12/5/20
+ * Question Description
+ Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+ Example 1:
+
+ Input: s = "eceba", k = 2
+ Output: 3
+ Explanation: T is "ece" which its length is 3.
+ Example 2:
+
+ Input: s = "aa", k = 1
+ Output: 2
+ Explanation: T is "aa" which its length is 2.
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O(n);
+ * Space complexity: O(1);
+
+ */
+public class _340_LongestSubstringwtihAtMostKDinstinceCharacters {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] words = new int[128];
+        int res = 0;
+        int num = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (words[s.charAt(i)]++ == 0) num++;
+            while (num > k) {
+                if (--words[s.charAt(j++)] == 0) {
+                    num--;
+                }
+            }
+            res = Math.max(res, i - j + 1);
+        }
+        return res;
+    }
+}
