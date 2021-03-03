e4c3d720fb93372123dabd6cde7d61cd723f6cb2
kqiu10 <qiukan97@gmail.com>
2021-03-03 16:18:05 -0500

    aded _42_

---
+++ leetcode二刷250/src/_41_FirstMissingPositive.java
@@ -0,0 +1,48 @@
+/**
+ * Date: 3/3/21
+ * Question Description:
+ * Given an unsorted integer array nums, find the smallest missing positive integer.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ * Input: nums = [1,2,0]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: nums = [3,4,-1,1]
+ * Output: 2
+ * Example 3:
+ *
+ * Input: nums = [7,8,9,11,12]
+ * Output: 1
+ *
+ */
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class _41_FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] - 1 != i) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+
+    }
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
