2106a14656c56b2fabe148532c7c13b1d63679e7
kqiu10 <qiukan97@gmail.com>
2021-03-04 12:54:24 -0500

    added _46_

---
+++ leetcode二刷250/src/_46_Permutations.java
@@ -0,0 +1,33 @@
+/**
+ * Date: 3/4/21
+ * Question Description:
+ * <p>
+ * Examples:
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Time Complexity: O(2^n)
+ * Space Complexity: O(n)
+ */
+public class _46_Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res, nums, new ArrayList<>());
+        return res;
+    }
+    private void helper(List<List<Integer>> res, int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            helper(res, nums, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
