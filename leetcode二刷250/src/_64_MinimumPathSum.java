b57d4fd21edf6b5395640af20d766b8bc4c498c6
kqiu10 <qiukan97@gmail.com>
2021-03-08 11:56:23 -0500

    added _64_

---
+++ leetcode二刷250/src/_64_MinimumPathSum.java
@@ -0,0 +1,45 @@
+/**
+ * Date: 3/8/21
+ * Question Description:
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom
+ * right, which minimizes the sum of all numbers along its path.
+ *
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ *
+ * Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * Output: 7
+ * Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+ * Example 2:
+ *
+ * Input: grid = [[1,2,3],[4,5,6]]
+ * Output: 12
+ */
+
+/**
+ * Time Complexity: O(m * n)
+ * Space Complexity: O(1)
+ */
+public class _64_MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
