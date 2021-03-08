1d3ea0777f44dad5943575f667b93b12d0b03bd5
kqiu10 <qiukan97@gmail.com>
2021-03-08 17:18:51 -0500

    added _70_

--- leetcode二刷250/src/_70_ClimbingStairs.java
+++ leetcode二刷250/src/_70_ClimbingStairs.java
@@ -6,11 +6,40 @@
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  * <p>
  * Examples:
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
  */
 
 /**
- * Time Complexity: O()
- * Space Complexity: O()
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
  */
 public class _70_ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int oneStep = 1;
+        int secondStep = 1;
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = oneStep + secondStep;
+            secondStep = oneStep;
+            oneStep = res;
+        }
+        return res;
+
+    }
+
 }
