e9f9b4f9b9875825362d5bab0843b5ac7b987163
kqiu10 <qiukan97@gmail.com>
2021-03-08 15:47:06 -0500

    added _68_

---
+++ leetcode二刷250/src/_68_TextJustification.java
@@ -0,0 +1,109 @@
+/**
+ * Date: 3/8/21
+ * Question Description:
+ * Given an array of words and a width maxWidth, format the text such that each line has exactly
+ * maxWidth characters and is fully (left and right) justified.
+ *
+ * You should pack your words in a greedy approach; that is, pack as many words as you can in
+ * each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+ *
+ * Extra spaces between words should be distributed as evenly as possible. If the number of
+ * spaces on a line do not divide evenly between words, the empty slots on the left will be
+ * assigned more spaces than the slots on the right.
+ *
+ * For the last line of text, it should be left justified and no extra space is inserted between
+ * words.
+ *
+ * Note:
+ *
+ * A word is defined as a character sequence consisting of non-space characters only.
+ * Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+ * The input array words contains at least one word.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ * Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+ * Output:
+ * [
+ *    "This    is    an",
+ *    "example  of text",
+ *    "justification.  "
+ * ]
+ * Example 2:
+ *
+ * Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+ * Output:
+ * [
+ *   "What   must   be",
+ *   "acknowledgment  ",
+ *   "shall be        "
+ * ]
+ * Explanation: Note that the last line is "shall be    " instead of "shall     be", because the
+ * last line must be left-justified instead of fully-justified.
+ * Note that the second line is also left-justified becase it contains only one word.
+ * Example 3:
+ *
+ * Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to",
+ * "a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+ * Output:
+ * [
+ *   "Science  is  what we",
+ *   "understand      well",
+ *   "enough to explain to",
+ *   "a  computer.  Art is",
+ *   "everything  else  we",
+ *   "do                  "
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class _68_TextJustification {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int index = 0;
+        while (index < words.length) {
+            int count = words[index].length();
+            int last = index + 1;
+            while (last < words.length) {
+                if (words[last].length() + count + 1 > maxWidth) break;
+                count += 1 + words[last].length();
+                last++;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(words[index]);
+            int dif = last - index - 1;
+            if (last == words.length || dif == 0) {
+                for (int i = index + 1; i < last; i++) {
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+                for (int i = sb.length(); i < maxWidth; i++) {
+                    sb.append(" ");
+                }
+            } else {
+                int spaces = (maxWidth - count) / dif;
+                int r = (maxWidth - count) % dif;
+                for (int i = index + 1; i < last; i++) {
+                    for (int k = spaces; k > 0; k--) {
+                        sb.append(" ");
+                    }
+                    if (r > 0) {
+                        sb.append(" ");
+                        r--;
+                    }
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+            }
+            res.add(sb.toString());
+            index = last;
+        }
+        return res;
+    }
+}
