ed1870ec6d6beda2ebf043402fd753dd04f8dc7e
kqiu10 <qiukan97@gmail.com>
2021-03-13 20:29:13 -0500

    added _76_

---
+++ leetcode二刷250/src/_76_MinimumWindowSubstring.java
@@ -0,0 +1,13 @@
+/**
+ * Date: 3/13/21
+ * Question Description:
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class _76_MinimumWindowSubstring {
+}
---
+++ leetcode二刷250/src/_82_RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,38 @@
+/**
+ * Date: 3/13/21
+ * Question Description:
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class _82_RemoveDuplicatesfromSortedListII {
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode deleteDuplicate(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p = dummy;
+        while (p.next != null && p.next.next != null) {
+            if (p.next.val == p.next.next.val) {
+                int minVal = p.next.val;
+                while (p.next != null && p.next.val == minVal) {
+                    p.next = p.next.next;
+                }
+            } else {
+                p = p.next;
+            }
+        }
+        return dummy.next;
+
+    }
+}
