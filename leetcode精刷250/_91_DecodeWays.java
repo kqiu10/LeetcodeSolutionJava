688c1ad0b734a9549d4f6f113b9b3af107e757fe
kqiu10 <qiukan97@gmail.com>
2021-03-15 23:02:39 -0400

    added _91_

--- leetcode精刷250/_91_DecodeWays.java
+++ leetcode精刷250/_91_DecodeWays.java
@@ -44,6 +44,7 @@ public class _91_DecodeWays {
         if (s == null || s.length() == 0) return 0;
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
+
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
         for (int i = 2; i <= s.length(); i++) {
             int first = Integer.valueOf(s.substring(i - 1, i));
