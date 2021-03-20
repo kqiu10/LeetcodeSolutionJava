c1a25a6f5b7af622c4cf122bbb963ef4a9e94dc3
kqiu10 <qiukan97@gmail.com>
2021-03-19 23:12:53 -0400

    added _112_

---
+++ leetcode二刷250/src/_112_PathSum.java
@@ -0,0 +1,49 @@
+/**
+ * Date: 3/19/21
+ * Question Description:
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a
+ * root-to-leaf path such that adding up all the values along the path equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ *
+ * Example 1:
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * Output: true
+ *
+ * Example 2:
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: false
+ *
+ * Example 3:
+ * Input: root = [1,2], targetSum = 0
+ * Output: false
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(h)
+ */
+public class _112_PathSum {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return targetSum == root.val;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+
+    }
+}
