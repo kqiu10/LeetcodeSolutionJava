8d84c02ace67b6702cffc4f43e7e46003dde5324
kqiu10 <qiukan97@gmail.com>
2021-03-21 20:20:11 -0400

    added _121_

---
+++ leetcode二刷250/src/_116_PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,23 @@
+/**
+ * Date: 3/21/21
+ * Question Description:
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent
+ * has two children. The binary tree has the following definition:
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class _116_PopulatingNextRightPointersinEachNode {
+
+}
