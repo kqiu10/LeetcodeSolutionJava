3262e9a78541bfc63103ee13dacc28f8dac0fa0d
kqiu10 <qiukan97@gmail.com>
2021-03-21 21:17:04 -0400

    added _116_

--- leetcode二刷250/src/_116_PopulatingNextRightPointersinEachNode.java
+++ leetcode二刷250/src/_116_PopulatingNextRightPointersinEachNode.java
@@ -19,5 +19,37 @@
  * Space Complexity: O()
  */
 public class _116_PopulatingNextRightPointersinEachNode {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+    public Node connect(Node root) {
+        if (root == null) return null;
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+        if (root.next != null && root.right != null) {
+            root.right.next = root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+
+    }
 
 }
