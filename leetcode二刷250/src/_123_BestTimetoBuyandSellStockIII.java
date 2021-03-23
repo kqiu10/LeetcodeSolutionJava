52097963e49c641050b9c77ca118199ce418c335
kqiu10 <qiukan97@gmail.com>
2021-03-22 20:34:56 -0400

    added _123_

---
+++ leetcode二刷250/src/_123_BestTimetoBuyandSellStockIII.java
@@ -0,0 +1,54 @@
+/**
+ * Date: 3/22/21
+ * Question Description:
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ *
+ * Find the maximum profit you can achieve. You may complete at most two transactions.
+ *
+ * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the
+ * stock before you buy again).
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ * Input: prices = [3,3,5,0,0,3,1,4]
+ * Output: 6
+ * Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ * Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+ * Example 2:
+ *
+ * Input: prices = [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging
+ * multiple transactions at the same time. You must sell before buying again.
+ * Example 3:
+ *
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * Example 4:
+ *
+ * Input: prices = [1]
+ * Output: 0
+ */
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class _123_BestTimetoBuyandSellStockIII {
+    public int maxProfit(int[] prices) {
+        int sell1 = 0;
+        int sell2 = 0;
+        int buy1 = Integer.MIN_VALUE;
+        int buy2 = Integer.MIN_VALUE;
+        for (int price : prices) {
+            sell2 = Math.max(sell2, buy2 + price);
+            buy2 = Math.max(buy2, -price + sell1);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy1 = Math.max(buy1, -price);
+        }
+        return sell2;
+    }
+}
