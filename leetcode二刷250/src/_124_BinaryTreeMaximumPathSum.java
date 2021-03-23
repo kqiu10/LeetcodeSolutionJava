c0f69605ab1de44877b9e2f650052b648648852c
kqiu10 <qiukan97@gmail.com>
2021-03-22 20:59:01 -0400

    added _124_

---
+++ leetcode二刷250/src/_124_BinaryTreeMaximumPathSum.java
@@ -0,0 +1,58 @@
+/**
+ * Date: 3/22/21
+ * Question Description:
+ * A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the
+ * sequence has an edge connecting them. A node can only appear in the sequence at most once.
+ * Note that the path does not need to pass through the root.
+ *
+ * The path sum of a path is the sum of the node's values in the path.
+ *
+ * Given the root of a binary tree, return the maximum path sum of any path.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3]
+ * Output: 6
+ * Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+ * Example 2:
+ *
+ *
+ * Input: root = [-10,9,20,null,null,15,7]
+ * Output: 42
+ * Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+ */
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class _124_BinaryTreeMaximumPathSum {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) return 0;
+        helper(root);
+        return res;
+    }
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        res = Math.max(res, left + right + root.val);
+        return Math.max(left, right) + root.val;
+    }
+}
