0dde4d19fdafb7fc9ec5d368a1759c980f81c90c
kqiu10 <qiukan97@gmail.com>
2021-03-23 23:08:09 -0400

    added _125_

--- leetcode二刷250/src/_125_ValidPalindrome.java
+++ leetcode二刷250/src/_125_ValidPalindrome.java
@@ -17,8 +17,6 @@
  * Explanation: "raceacar" is not a palindrome.
  */
 
-import java.util.Scanner;
-
 /**
  * Time Complexity: O(n)
  * Space Complexity: O(1)
@@ -41,19 +39,5 @@ public class _125_ValidPalindrome {
         return true;
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int score = scan.nextInt();
-        System.out.println("whats the profit: " + score);
-
-        switch (score ) {
-            case 1:
-                System.out.println("1");
-            case 2:
-                System.out.println("2");
-            default:
-                System.out.println("end");
-        }
-    }
 
 }
