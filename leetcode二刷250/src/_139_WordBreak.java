7499f5b542d4618136f62a3057b7a268315f0815
kqiu10 <qiukan97@gmail.com>
2021-03-31 23:24:03 -0400

    added _139_

---
+++ leetcode二刷250/src/_139_WordBreak.java
@@ -0,0 +1,48 @@
+/**
+ * Date: 3/31/21
+ * Question Description:
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into
+ * a space-separated sequence of one or more dictionary words.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple","pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ * Output: false
+ */
+
+import java.util.List;
+
+/**
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ */
+public class _139_WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+}
