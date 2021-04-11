084a3ba5dc92b1dc1cdd5b580a17fd16099b8a5e
kqiu10 <qiukan97@gmail.com>
2021-04-10 21:08:19 -0400

    added Beaconfire test2

--- leetcode二刷250/src/test.java
+++ leetcode二刷250/src/test.java
@@ -6,6 +6,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,9 +29,29 @@ public class test {
         return new ArrayList<Integer>(res);
     }
 
+    public void swapZero(int[] arr) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (l < r) {
+            while (arr[r] == 0) {
+                r--;
+            }
+            if (l < r && arr[l] == 0) {
+                swap(l++, r--, arr);
+            }
+            l++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    void swap(int a, int b, int[] arr) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
     public static void main(String[] args) {
         test t1 = new test();
-        System.out.println( t1.findCommon(new int[]{1, 2, 5, 3, 0}, new int[]{2, 2, 0}));
-
+//        System.out.println( t1.findCommon(new int[]{1, 2, 5, 3, 0}, new int[]{2, 2, 0}));
+        t1.swapZero(new int[]{0, 8, 9, 0, 4, 2, 0, 0});
     }
 }
