3d9eb09d2456ae6cd62392c287a5315fe8eb0222
kqiu10 <qiukan97@gmail.com>
2021-04-20 16:21:53 -0400

    finished LicenseKeyFormatting

---
+++ leetcode二刷250/src/com/google/src/LicenseKeyFormatting.java
@@ -0,0 +1,47 @@
+package com.google.src;
+/**
+ * Date: 4/20/21
+ * Question Description:
+ * You are given a license key represented as a string s that consists of only alphanumeric
+ * characters and dashes. The string is separated into n + 1 groups by n dashes. You are also
+ * given an integer k.
+ *
+ * We want to reformat the string s such that each group contains exactly k characters, except
+ * for the first group, which could be shorter than k but still must contain at least one
+ * character. Furthermore, there must be a dash inserted between two groups, and you should
+ * convert all lowercase letters to uppercase.
+ *
+ * Return the reformatted license key.
+ * <p>
+ * Examples:
+ * Example 1:
+ *
+ * Input: s = "5F3Z-2e-9-w", k = 4
+ * Output: "5F3Z-2E9W"
+ * Explanation: The string s has been split into two parts, each part has 4 characters.
+ * Note that the two extra dashes are not needed and can be removed.
+ * Example 2:
+ *
+ * Input: s = "2-5g-3-J", k = 2
+ * Output: "2-5G-3J"
+ * Explanation: The string s has been split into three parts, each part has 2 characters except
+ * the first part
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class LicenseKeyFormatting {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length() - 1;
+        for (int i = len; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                sb.append(sb.length() % (k + 1) == k ? "-" : "").append(s.charAt(i));
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+
+}
