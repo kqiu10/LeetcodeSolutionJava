a87a125789dd5736a5091d0e3f998389d9618d38
kqiu10 <qiukan97@gmail.com>
2021-04-21 10:01:19 -0400

    finished _1219_PathWithMaximumGold

---
+++ leetcode二刷250/src/com/google/src/_1631_PathWithMinimumEffort.java
@@ -0,0 +1,25 @@
+package com.google.src;
+/**
+ * Date: 4/21/21
+ * Question Description:
+ * You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows
+ * x columns, where heights[row][col] represents the height of cell (row, col). You are situated
+ * in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1,
+ * columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a
+ * route that requires the minimum effort.
+ *
+ * A route's effort is the maximum absolute difference in heights between two consecutive cells
+ * of the route.
+ *
+ * Return the minimum effort required to travel from the top-left cell to the bottom-right cell.
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class _1631_PathWithMinimumEffort {
+
+}
