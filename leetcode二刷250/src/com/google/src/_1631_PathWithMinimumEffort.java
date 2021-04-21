bee4bdca46b8b492e65736b41d5996cdc30086db
kqiu10 <qiukan97@gmail.com>
2021-04-21 10:36:11 -0400

    finished _1631_PathWithMinimumEffort

--- leetcode二刷250/src/com/google/src/_1631_PathWithMinimumEffort.java
+++ leetcode二刷250/src/com/google/src/_1631_PathWithMinimumEffort.java
@@ -16,10 +16,41 @@ package com.google.src;
  * Examples:
  */
 
+import java.util.PriorityQueue;
+
 /**
- * Time Complexity: O()
- * Space Complexity: O()
+ * Time Complexity: O(nlogn)
+ * Space Complexity: O(n)
  */
 public class _1631_PathWithMinimumEffort {
+    public int minimumEffortPath(int[][] heights) {
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int m = heights.length;
+        int n = heights[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        Integer[][] minDist = new Integer[m][n];
+        minDist[0][0] = 0;
+        pq.offer(new int[]{0, 0, 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            if (x == m - 1 && y == n - 1) {
+                return cur[2];
+            }
+            for (int[] dir : dirs) {
+                int nx = dir[0] + x;
+                int ny = dir[1] + y;
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                int effort = Math.max(cur[2], Math.abs(heights[cur[0]][cur[1]] - heights[nx][ny]));
+                if (minDist[nx][ny] == null || minDist[nx][ny] > effort) {
+                    minDist[nx][ny] = effort;
+                    pq.offer(new int[]{nx, ny, minDist[nx][ny]});
+                }
+
+            }
+        }
+        return -1;
+    }
 
 }
