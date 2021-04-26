8d0ceef95fdc6b3582ec048d08aef85e0b2e386c
kqiu10 <qiukan97@gmail.com>
2021-04-26 09:26:52 -0400

    finished _88_MergeSortedArray

---
+++ leetcode二刷250/src/com/antra/_88_MergeSortedArray.java
@@ -0,0 +1,30 @@
+package com.antra;
+/**
+ * Date: 4/25/21
+ * Question Description:
+ * <p>
+ * Examples:
+ */
+
+/**
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+public class _88_MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int e1 = m - 1;
+        int e2 = n - 1;
+        int end = m + n - 1;
+        while (e1 >= 0 || e2 >= 0) {
+            while (e1 >= 0 && e2 >= 0) {
+                nums1[end--] = nums1[e1] > nums2[e2] ? nums1[e1--] : nums2[e2--];
+            }
+            if (e1 >= 0) {
+                nums1[end--] = nums1[e1--];
+            }
+            if (e2 >= 0) {
+                nums1[end--] = nums2[e2--];
+            }
+        }
+    }
+}
